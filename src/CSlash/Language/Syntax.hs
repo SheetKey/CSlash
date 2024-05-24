74a2ef0221c99bc08cdfe97f81db0f58608e98ba
Will <wveatchw@gmail.com>
2024-05-24 17:55:04 -0400

    syntax

--- src/CSlash/Language/Syntax.hs
+++ src/CSlash/Language/Syntax.hs
@@ -1,2 +1,14 @@
-module CSlash.Language.Syntax where
+module CSlash.Language.Syntax (
+  ModuleName(..), CsModule(..)
+  ) where
 
+import CSlash.Language.Extension
+import CSlash.Language.Module.Name
+
+-- | Top-level structure of a module.
+data CsModule p = CsModule
+  { csmodName :: Maybe (XRec p ModuleName)
+  , hsmodExports :: Maybe (XRec p [LIE p])
+  , hsmodImports :: [LImportDecl p]
+  , hsmodDecls :: [LDecl p]
+  }
