763a6f9b1e6c87ae8dc565694236b67cd9bbf057
Will <wveatchw@gmail.com>
2024-05-24 16:24:10 -0400

    rename

--- src/CSlash/Language/Module/Name.hs
+++
@@ -1,3 +0,0 @@
-module CSlash.Language.Module.Name (X) where
-
-import Haskell.Language.Module.Name as X
---
+++ src/CSlash/Language/Syntax/Module/Name.hs
@@ -0,0 +1,3 @@
+module CSlash.Language.Syntax.Module.Name (X) where
+
+import Haskell.Language.Syntax.Module.Name as X
