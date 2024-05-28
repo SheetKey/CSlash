a6beb32df418de2d4d46831b1ffe13ef5b7b01a9
Will <wveatchw@gmail.com>
2024-05-28 15:16:32 -0400

    kind

---
+++ src/CSlash/Language/Syntax/Kind.hs
@@ -0,0 +1,26 @@
+module CSlash.Language.Syntax.Kind where
+
+import CSlash.Language.Syntax.Extension
+
+type LCsKind pass = XRec pass (CsKind pass)
+
+data CsKind pass
+  = CsUKd (XUKd pass)
+  | CsAKd (XAKd pass)
+  | CsLKd (XLKd pass)
+  | CsKdVar (XKdVar pass) (LIdP pass)
+  | CsFunKd (XFunKd pass) (LCsKind pass) (LCsKind pass)
+  | CsQualKd
+    { csk_xqual :: XQualKd pass
+    , csk_ctxt :: LCsContext pass
+    , csk_body :: LCsKind pass
+    }
+  | CsParKd (XParKd pass) (LCsKind pass)
+
+type LCsContext pass = XRec pass (CsContext pass)
+
+type CsContext pass = [CsKdRel pass]
+
+data CsKdRel pass
+  = CsKdLT (XKdLT pass) (LCsKind pass) (LCsKind pass)
+  | CsKdLTEQ (XKdLTEQ pass) (LCsKind pass) (LCsKind pass)
