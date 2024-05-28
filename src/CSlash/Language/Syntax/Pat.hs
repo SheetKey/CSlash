397e8c594e75f94b0e457baec4df7bc3bacb1142
Will <wveatchw@gmail.com>
2024-05-28 15:13:37 -0400

    pat

--- src/CSlash/Language/Syntax/Pat.hs
+++ src/CSlash/Language/Syntax/Pat.hs
@@ -1,2 +1,21 @@
 module CSlash.Language.Syntax.Pat where
 
+import CSlash.Language.Syntax.Extension
+
+type LPat p = XRec p (Pat p)
+
+type ConTag = Int
+
+type SumWidth = Int
+
+data Pat p
+  ------------ Simple patterns ---------------
+  = WildPat (XWildPat p)
+  | VarPat (XVarPat p) (LIdP p)
+  ------------ Tuples, sums ---------------
+  | TuplePat (XTuplePat p) [LPat p]
+  | SumPat (XSumPat p) (LPat p) ConTag SumWidth
+  ------------ Literals ---------------
+  | LitPat (XLitPat p) (CsLit p)
+  ------------ With type signature ---------------
+  | SigPat (XSigPat p) (Lpat p) (CsPatSigType (NoTc p))
