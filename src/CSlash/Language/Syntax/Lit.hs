f0e9befd13a4f75ebb5aaad85915233aca2e22a7
Will <wveatchw@gmail.com>
2024-05-28 15:13:45 -0400

    lit

--- src/CSlash/Language/Syntax/Lit.hs
+++ src/CSlash/Language/Syntax/Lit.hs
@@ -1,1 +1,13 @@
 module CSlash.Language.Syntax.Lit where
+
+import CSlash.Language.Syntax.Extension
+
+import CSlash.Types.SourceText
+
+import CSlash.Data.FastString
+
+data CsLit x
+  = CsChar (XCsChar x) {- SourceText -} Char
+  | CsString (XCsString x) {- SourceText -} FastString
+  | CsInt (XCsInt x) IntegralLit
+  | CsDouble (XCsDouble x) FractionalLit
