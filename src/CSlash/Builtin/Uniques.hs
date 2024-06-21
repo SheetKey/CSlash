570a7f70300eb764b5e5b3aa03d6a3a18a58aca1
Will <wveatchw@gmail.com>
2024-06-21 16:13:51 -0400

    Builtin.Uniques

--- src/CSlash/Builtin/Uniques.hs
+++ src/CSlash/Builtin/Uniques.hs
@@ -1,6 +1,29 @@
 module CSlash.Builtin.Uniques where
 
+import CSlash.Types.Basic
 import CSlash.Types.Unique
+
+import CSlash.Utils.Word64 (word64ToInt)
+
+import Data.Bits ((.&.), shiftR)
+
+isSumTyConUnique :: Unique -> Maybe Arity
+isSumTyConUnique u =
+  case (tag, n .&. 0xfc) of
+    ('z', 0xfc) -> Just (word64ToInt n `shiftR` 8)
+    _ -> Nothing
+  where
+    (tag, n) = unpkUnique u
+
+isTupleTyConUnique :: Unique -> Maybe Arity
+isTupleTyConUnique u =
+  case (tag, i) of
+    ('4', 0) -> Just arity
+    _ -> Nothing
+  where
+    (tag, n) = unpkUnique u
+    (arity', i) = quotRem n 2
+    arity = word64ToInt arity'
 
 varNSUnique :: Unique
 varNSUnique = mkUnique 'i' 0
