87dce5684a134bdd45051b6307a5150192a700e5
Will <wveatchw@gmail.com>
2024-06-21 16:16:19 -0400

    plain panic

--- src/CSlash/Utils/Panic/Plain.hs
+++ src/CSlash/Utils/Panic/Plain.hs
@@ -1,3 +1,61 @@
-module CSlash.Utils.Panic.Plain ( module X ) where
+{-# LANGUAGE LambdaCase #-}
 
-import GHC.Utils.Panic.Plain as X
+module CSlash.Utils.Panic.Plain where
+
+import CSlash.Stack
+import CSlash.Utils.Constants
+import CSlash.Utils.Exception as Exception
+import CSlash.Version
+
+data PlainCsException
+  = PlainSignal Int
+  | PlainUsageError String
+  | PlainCmdLineError String
+  | PlainPanic String
+  | PlainSorry String
+  | PlainInstallationError String
+  | PlainProgramError String
+
+instance Exception PlainCsException
+
+instance Show PlainCsException where
+  showsPrec _ e = showPlainCsException e
+
+short_usage :: String
+short_usage = "Usage: For basic information, try the `--help' option."
+
+showPlainCsException :: PlainCsException -> ShowS
+showPlainCsException =
+  \case
+    PlainSignal n -> showString "signal: " . shows n
+    PlainUsageError str -> showString str . showChar '\n' . showString short_usage
+    PlainCmdLineError str -> showString str
+    PlainPanic s -> panicMsg (showString s)
+    PlainSorry s -> sorryMsg (showString s)
+    PlainInstallationError str -> showString str
+    PlainProgramError str -> showString str
+  where
+    sorryMsg :: ShowS -> ShowS
+    sorryMsg s =
+        showString "sorry! (unimplemented feature or known bug)\n"
+      . showString ("  CSlash version " ++ cProjectVersion ++ ":\n\t")
+      . s . showString "\n"
+    panicMsg :: ShowS -> ShowS
+    panicMsg s =
+        showString "panic! (then 'impossible' happened)\n"
+      . showString ("  CSlash version " ++ cProjectVersion ++ ":\n\t")
+      . s . showString "\n"
+
+assertPanic' :: HasCallStack => a
+assertPanic' =
+  let doc = unlines $ fmap ("  "++) $ lines (prettyCallStack callStack)
+  in Exception.throw (Exception.AssertionFailed
+                       ("ASSERT failed!\n"
+                       ++ withFrozenCallStack doc))
+
+assert :: HasCallStack => Bool -> a -> a
+{-# INLINE assert #-}
+assert cond a =
+  if debugIsOn && not cond
+  then withFrozenCallStack assertPanic'
+  else a
