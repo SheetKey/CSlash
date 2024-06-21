a7f3655654a2bdfc83576ef8d6253cced5f5ae35
Will <wveatchw@gmail.com>
2024-06-21 16:15:07 -0400

    errors.types

--- src/CSlash/Parser/Errors/Types.hs
+++ src/CSlash/Parser/Errors/Types.hs
@@ -1,3 +1,105 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module CSlash.Parser.Errors.Types where
 
+-- base
+import Data.List.NonEmpty
+
+-- compiler
+import CSlash.Data.FastString
+
+import CSlash.Cs.Extension
+
+import CSlash.Types.Error
+import CSlash.Types.Name.Reader
+import CSlash.Types.Name.Occurrence
+import CSlash.Types.SrcLoc
+
+import CSlash.Parser.Errors.Basic
+import CSlash.Parser.Types
+
+import CSlash.Language.Syntax.Expr
+import CSlash.Language.Syntax.Type
+import CSlash.Language.Syntax.Pat
+
+-- ghc
+import GHC.Generics (Generic)
+
+data PsMessage
+  = PsUnknownMessage (UnknownDiagnostic (DiagnosticOpts PsMessage))
+  | PsWarnBidirectionalFormatChars (NonEmpty (PsLoc, Char, String))
+  | PsWarnTab !Word
+  | PsWarnOperatorWhitespaceExtConflict !OperatorWhitespaceSymbol
+  | PsWarnOperatorWhitespace !FastString !OperatorWhitespaceSymbol
+  | PsErrEmptyLambda
+  | PsErrPrimStringInvalidChar
+  | PsErrMissingBlock
+  | PsErrLexer !LexErr !LexErrKind
+  | PsErrUnexpectedQualifiedConstructor !RdrName
+  | PsErrTupleSectionInPat
+  | PsErrOpFewArgs !RdrName
+  | PsErrImportQualifiedTwice
+  | PsErrImportPreQualified
+  | PsErrVarForTyCon !RdrName
+  | PsErrPrecedenceOutOfRange !Int
+  | PsErrIfInPat -- replaces PsErrIfThenElseInPat
+  | PsErrLambdaInPat 
+  | PsErrCaseInPat
+  | PsErrLetInPat
+  | PsErrInvalidInfixHole
+  | PsErrCaseInFunAppExpr !(LCsExpr Ps)
+  | PsErrLambdaInFunAppExpr !(LCsExpr Ps)
+  | PsErrLetInFunAppExpr !(LCsExpr Ps)
+  | PsErrIfInFunAppExpr !(LCsExpr Ps)
+  | PsErrParseErrorOnInput !OccName
+  | PsErrInvalidTypeSignature !PsInvalidTypeSignature !(LCsExpr Ps)
+  | PsErrUnexpectedTypeInDecl !(LCsType Ps)
+                              !SDoc
+                              !RdrName
+                              [LCsTypeArg Ps]
+                              !SDoc
+  | PsErrInPat !(PatBuilder Ps) !PsErrInPatDetails
+  | PsErrUnicodeCharLooksLike Char Char String
+  deriving Generic
+
 data PsErrParseDetails = PsErrParseDetails
+
+data PsInvalidTypeSignature
+  = PsErrInvalidTypeSig_Qualified
+  | PsErrInvalidTypeSig_DataCon
+  | PsErrInvalidTypeSig_Other
+
+data PatIsRecursive
+  = YesPatIsRecursive
+  | NoPatIsRecursive
+
+data ParseContext = ParseContext
+  { is_infix :: !(Maybe RdrName)
+  }
+  deriving (Eq)
+
+data PsErrInPatDetails
+  = PEIP_NegApp
+  | PEIP_TypeArgs [CsConPatTyArg Ps]
+  | PEIP_RecPattern [LPat Ps]
+                    !PatIsRecursive
+                    !ParseContext
+  | PEIP_OtherPatDetails !ParseContext
+
+noParseContext :: ParseContext
+noParseContext = ParseContext Nothing
+
+fromParseContext :: ParseContext -> PsErrInPatDetails
+fromParseContext = PEIP_OtherPatDetails
+
+data LexErrKind
+  = LexErrKind_EOF
+  | LexErrKind_UTF8
+  | LexErrKind_Char !Char
+  deriving (Show, Eq, Ord)
+
+data LexErr
+  = LexError
+  | LexStringCharLit
+  | LexStringCharLitEOF
+  | LexUnterminatedComment
