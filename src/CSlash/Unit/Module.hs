5b7be388f6677b995c561fca153022724261681e
Will <wveatchw@gmail.com>
2024-06-21 16:16:02 -0400

    module

--- src/CSlash/Unit/Module.hs
+++ src/CSlash/Unit/Module.hs
@@ -3,5 +3,5 @@ module CSlash.Unit.Module where
 import CSlash.Unit.Types
 
 isHoleModule :: GenModule (GenUnit u) -> Bool
-isHoleModule (Moudle HoleUnit _) = True
+isHoleModule (Module HoleUnit _) = True
 isHoleModule _ = False
