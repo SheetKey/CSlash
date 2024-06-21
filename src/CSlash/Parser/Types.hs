9c8deaad2da33131bcc033ce753271a316bf9897
Will <wveatchw@gmail.com>
2024-06-21 16:15:20 -0400

    parser.types

--- src/CSlash/Parser/Types.hs
+++ src/CSlash/Parser/Types.hs
@@ -1,12 +1,24 @@
+{-# LANGUAGE DataKinds #-}
+
 module CSlash.Parser.Types
   ( PatBuilder(..) )
   where
+
+import CSlash.Cs.Extension
+
+import CSlash.Parser.Annotation
+
+import CSlash.Types.Name.Reader
+
+import CSlash.Language.Syntax.Type
+import CSlash.Language.Syntax.Pat
+import CSlash.Language.Syntax.Lit
 
 data PatBuilder p
   = PatBuilderPat (Pat p)
   | PatBuilderPar (EpToken "(") (LocatedA (PatBuilder p)) (EpToken ")")
   | PatBuilderApp (LocatedA (PatBuilder p)) (LocatedA (PatBuilder p))
-  | PatBuilderAppType (LocatedA (PatBuilder p)) (CsTyPar Ps)
+  | PatBuilderAppType (LocatedA (PatBuilder p)) (CsTyPat Ps)
   | PatBuilderOpApp (LocatedA (PatBuilder p)) (LocatedN RdrName)
                     (LocatedA (PatBuilder p)) [AddEpAnn]
   | PatBuilderVar (LocatedN RdrName)
