a3ed6fbf3676e5021b1a0eb59191cae22387c94e
Will <wveatchw@gmail.com>
2024-06-22 16:26:34 -0400

    greinfo

---
+++ src/CSlash/Types/GREInfo.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DerivingStrategies #-}
+
+module CSlash.Types.GREInfo where
+
+import CSlash.Types.Basic
+import CSlash.Types.Name
+import CSlash.Types.Unique
+import CSlash.Types.Unique.Set
+import CSlash.Utils.Outputable
+import CSlash.Utils.Panic
+
+import Data.Data ( Data )
+import Data.List.NonEmpty ( NonEmpty )
+import qualified Data.List.NonEmpty as NonEmpty
+
+data GREInfo
+    = Vanilla
+    | UnboundGRE
+    | IAmTyCon    !(TyConFlavour Name)
+    | IAmConLike  !ConInfo
+    deriving Data
+
+instance NFData GREInfo where
+  rnf Vanilla = ()
+  rnf UnboundGRE = ()
+  rnf (IAmTyCon tc) = rnf tc
+  rnf (IAmConLike info) = rnf info
+  rnf (IAmRecField info) = rnf info
+
+plusGREInfo :: GREInfo -> GREInfo -> GREInfo
+plusGREInfo Vanilla Vanilla = Vanilla
+plusGREInfo UnboundGRE UnboundGRE = UnboundGRE
+plusGREInfo (IAmTyCon {})    info2@(IAmTyCon {}) = info2
+plusGREInfo (IAmConLike {})  info2@(IAmConLike {}) = info2
+plusGREInfo (IAmRecField {}) info2@(IAmRecField {}) = info2
+plusGREInfo info1 info2 = pprPanic "plusInfo" $
+  vcat [ text "info1:" <+> ppr info1
+       , text "info2:" <+> ppr info2 ]
+
+instance Outputable GREInfo where
+  ppr Vanilla = text "Vanilla"
+  ppr UnboundGRE = text "UnboundGRE"
+  ppr (IAmTyCon flav)
+    = text "TyCon" <+> ppr flav
+  ppr (IAmConLike info)
+    = text "ConLike" <+> ppr info
+  ppr (IAmRecField info)
+    = text "RecField" <+> ppr info
+
+data ConInfo
+  = ConHasPositionalArgs
+  | ConIsNullary
+  deriving stock Eq
+  deriving Data
+
+mkConInfo :: Arity -> ConInfo
+mkConInfo 0 = ConIsNullary
+mkConInfo _ = ConHasPositionalArgs
+
+instance Outputable ConInfo where
+  ppr ConIsNullary = text "ConIsNullary"
+  ppr ConHasPositionalArgs = text "ConHasPositionalArgs"
