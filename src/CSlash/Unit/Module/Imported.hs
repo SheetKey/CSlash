f4339076f4b2cb6351d54f68f60e00ca09c90202
Will <wveatchw@gmail.com>
2024-06-22 15:11:21 -0400

    Unit.Module.Imported

---
+++ src/CSlash/Unit/Module/Imported.hs
@@ -0,0 +1,30 @@
+module CSlash.Unit.Module.Imported
+  ( ImportedMods
+  , ImportedBy (..)
+  , ImportedModsVal (..)
+  , importedByUser
+  ) where
+
+import CSlash.Unit.Module
+
+import CSlash.Types.Name.Reader
+import CSlash.Types.SrcLoc
+
+type ImportedMods = ModuleEnv [ImportedBy]
+
+data ImportedBy
+    = ImportedByUser ImportedModsVal
+    | ImportedBySystem
+
+importedByUser :: [ImportedBy] -> [ImportedModsVal]
+importedByUser (ImportedByUser imv : bys) = imv : importedByUser bys
+importedByUser (ImportedBySystem   : bys) =       importedByUser bys
+importedByUser [] = []
+
+data ImportedModsVal = ImportedModsVal
+   { imv_name        :: ModuleName
+   , imv_span        :: SrcSpan
+   , imv_is_hiding   :: Bool
+   , imv_all_exports :: !GlobalRdrEnv
+   , imv_qualified   :: Bool
+   }
