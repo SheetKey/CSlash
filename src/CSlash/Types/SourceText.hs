f34e9ddd076f5e9e4e34be474418e6232707a15a
Will <wveatchw@gmail.com>
2024-06-30 19:17:40 -0400

    types.sourcetext

--- src/CSlash/Types/SourceText.hs
+++ src/CSlash/Types/SourceText.hs
@@ -1,8 +1,27 @@
 module CSlash.Types.SourceText
-  ( SourceText(..)
-  , IntegralLit(..)
-  , FractionalLit(..)
-  , FractionalExponentBase(..)
+  ( module X
+  , pprWithSourceText
   ) where
 
-import GHC.Types.SourceText
+import GHC.Types.SourceText as X hiding
+  (pprWithSourceText)
+
+import CSlash.Utils.Outputable
+
+pprWithSourceText :: SourceText -> SDoc -> SDoc
+pprWithSourceText NoSourceText d = d
+pprWithSourceText (SourceText src) _ = ftext src
+
+
+mkRationalWithExponentBase :: Rational -> Integer -> FractionalExponentBase -> Rational
+mkRationalWithExponentBase i e feb = i * (eb ^^ e)
+  where eb = case feb of Base2 -> 2 ; Base10 -> 10
+
+instance Outputable StringLiteral where
+  ppr sl = pprWithSourceText (sl_st sl) (doubleQuotes $ ftext $ sl_fs sl)
+
+instance Outputable FractionalLit where
+  ppr fl@(FL {}) =
+    pprWithSourceText (fl_text fl) $
+    rational $ mkRationalWithExponentBase (fl_signi fl) (fl_exp fl) (fl_exp_base fl)
+
