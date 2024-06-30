a35a0ac818392214da7e82092805ed24db01cbbb
Will <wveatchw@gmail.com>
2024-06-30 19:17:01 -0400

    types.greinfo

--- src/CSlash/Types/GREInfo.hs
+++ src/CSlash/Types/GREInfo.hs
@@ -10,6 +10,7 @@ import CSlash.Types.Unique.Set
 import CSlash.Utils.Outputable
 import CSlash.Utils.Panic
 
+import Control.DeepSeq
 import Data.Data ( Data )
 import Data.List.NonEmpty ( NonEmpty )
 import qualified Data.List.NonEmpty as NonEmpty
@@ -26,14 +27,12 @@ instance NFData GREInfo where
   rnf UnboundGRE = ()
   rnf (IAmTyCon tc) = rnf tc
   rnf (IAmConLike info) = rnf info
-  rnf (IAmRecField info) = rnf info
 
 plusGREInfo :: GREInfo -> GREInfo -> GREInfo
 plusGREInfo Vanilla Vanilla = Vanilla
 plusGREInfo UnboundGRE UnboundGRE = UnboundGRE
 plusGREInfo (IAmTyCon {})    info2@(IAmTyCon {}) = info2
 plusGREInfo (IAmConLike {})  info2@(IAmConLike {}) = info2
-plusGREInfo (IAmRecField {}) info2@(IAmRecField {}) = info2
 plusGREInfo info1 info2 = pprPanic "plusInfo" $
   vcat [ text "info1:" <+> ppr info1
        , text "info2:" <+> ppr info2 ]
@@ -45,14 +44,16 @@ instance Outputable GREInfo where
     = text "TyCon" <+> ppr flav
   ppr (IAmConLike info)
     = text "ConLike" <+> ppr info
-  ppr (IAmRecField info)
-    = text "RecField" <+> ppr info
 
 data ConInfo
   = ConHasPositionalArgs
   | ConIsNullary
   deriving stock Eq
   deriving Data
+
+instance NFData ConInfo where
+  rnf ConHasPositionalArgs = ()
+  rnf ConIsNullary = ()
 
 mkConInfo :: Arity -> ConInfo
 mkConInfo 0 = ConIsNullary
