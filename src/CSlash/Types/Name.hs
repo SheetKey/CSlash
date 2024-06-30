863e629839d96eb733a76ed711501fa369869b9e
Will <wveatchw@gmail.com>
2024-06-30 19:17:15 -0400

    types.name

--- src/CSlash/Types/Name.hs
+++ src/CSlash/Types/Name.hs
@@ -1,4 +1,49 @@
-module CSlash.Types.Name where
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module CSlash.Types.Name
+  ( Name,
+    BuiltInSyntax(..),
+    mkSystemName, mkSystemNameAt,
+    mkInternalName, mkClonedInternalName, mkDerivedInternalName,
+    mkSystemVarName, mkSysTvName,
+    mkFCallName,
+    mkExternalName, mkWiredInName,
+    
+    nameUnique, setNameUnique,
+    nameOccName, nameNameSpace, nameModule, nameModule_maybe,
+    setNameLoc,
+    tidyNameOcc,
+    localiseName,
+    namePun_maybe,
+
+    pprName,
+    nameSrcLoc, nameSrcSpan, pprNameDefnLoc, pprDefinedAt,
+    pprFullName, pprTickyName,
+
+    isSystemName, isInternalName, isExternalName,
+    isTyVarName, isTyConName,
+    -- isDataConName,
+    isValName, isVarName,
+    -- isDynLinkName, isFieldName,
+    isWiredInName, isWiredIn, isBuiltInSyntax, isTupleTyConName,
+    isSumTyConName,
+    -- isUnboxedTupleDataConLikeName,
+    isHoleName,
+    wiredInNameTyThing_maybe,
+    nameIsLocalOrFrom, nameIsExternalOrFrom, nameIsHomePackage,
+    nameIsHomePackageImport, nameIsFromExternalPackage,
+    stableNameCmp,
+
+    NamedThing(..),
+    getSrcLoc, getSrcSpan, getOccString, getOccFS,
+
+    pprInfixName, pprPrefixName, pprModulePrefix, pprNameUnqualified,
+    nameStableString,
+
+    module CSlash.Types.Name.Occurrence
+) where
 
 import Prelude hiding ((<>))
 
@@ -8,14 +53,20 @@ import CSlash.Types.Name.Occurrence
 import CSlash.Types.SrcLoc
 import CSlash.Types.TyThing
 import CSlash.Types.Unique
+import CSlash.Unit.Home
 import CSlash.Unit.Types
 import CSlash.Unit.Module
 import CSlash.Utils.Misc
 import CSlash.Utils.Outputable
 import CSlash.Utils.Panic
+import CSlash.Utils.Binary
 import CSlash.Builtin.Uniques (isTupleTyConUnique, isSumTyConUnique)
 
 import Data.List ( intersperse )
+
+import Control.DeepSeq
+import Data.Data
+import qualified Data.Semigroup as S
 
 data Name = Name
   { n_sort :: NameSort
@@ -36,15 +87,70 @@ instance Outputable NameSort where
   ppr Internal = text "internal"
   ppr System = text "system"
 
+instance NFData Name where
+  rnf Name{..} = rnf n_sort `seq` rnf n_occ `seq` n_uniq `seq` rnf n_loc
+
+instance NFData NameSort where
+  rnf (External m) = rnf m
+  rnf (WiredIn m t b) = rnf m `seq` t `seq` b `seq` ()
+  rnf Internal = ()
+  rnf System = ()
+
 data BuiltInSyntax = BuiltInSyntax | UserSyntax
+
+instance HasOccName Name where
+  occName = nameOccName
 
 nameUnique :: Name -> Unique
 nameUnique name = n_uniq name
+
+nameOccName :: Name -> OccName
+nameOccName name = n_occ name
+
+nameNameSpace :: Name -> NameSpace
+nameNameSpace name = occNameSpace (n_occ name)
+
+nameSrcLoc :: Name -> SrcLoc
+nameSrcLoc name = srcSpanStart (n_loc name)
+
+nameSrcSpan :: Name -> SrcSpan
+nameSrcSpan name = n_loc name
+
+isInternalName :: Name -> Bool
+isInternalName name = not (isExternalName name)
 
 isExternalName :: Name -> Bool
 isExternalName (Name {n_sort = External _}) = True
 isExternalName (Name {n_sort = WiredIn _ _ _}) = True
 isExternalName _ = False
+
+isSystemName :: Name -> Bool
+isSystemName (Name {n_sort = System}) = True
+isSystemName _                        = False
+
+isWiredInName :: Name -> Bool
+isWiredInName (Name {n_sort = WiredIn _ _ _}) = True
+isWiredInName _ = False
+
+isWiredIn :: NamedThing thing => thing -> Bool
+isWiredIn = isWiredInName . getName
+
+wiredInNameTyThing_maybe :: Name -> Maybe TyThing
+wiredInNameTyThing_maybe (Name {n_sort = WiredIn _ thing _}) = Just thing
+wiredInNameTyThing_maybe _ = Nothing
+
+isBuiltInSyntax :: Name -> Bool
+isBuiltInSyntax (Name {n_sort = WiredIn _ _ BuiltInSyntax}) = True
+isBuiltInSyntax _                                           = False
+
+isTupleTyConName :: Name -> Bool
+isTupleTyConName = isJust . isTupleTyConUnique . getUnique
+
+isSumTyConName :: Name -> Bool
+isSumTyConName = isJust . isSumTyConUnique . getUnique
+
+isHoleName :: Name -> Bool
+isHoleName = isHoleModule . nameModule
 
 nameModule :: HasDebugCallStack => Name -> Module
 nameModule name =
@@ -55,6 +161,9 @@ nameModule_maybe :: Name -> Maybe Module
 nameModule_maybe (Name {n_sort = External mod}) = Just mod
 nameModule_maybe (Name {n_sort = WiredIn mod _ _}) = Just mod
 nameModule_maybe _ = Nothing
+
+is_from :: Module -> Module -> Bool
+is_from from mod = from == mod
 
 namePun_maybe :: Name -> Maybe FastString
 namePun_maybe name
@@ -68,11 +177,141 @@ namePun_maybe name
     bars ar = intersperse ' ' (replicate (ar-1) '|')
 namePun_maybe _ = Nothing    
 
-nameOccName :: Name -> OccName
-nameOccName name = n_occ name
+nameIsLocalOrFrom :: Module -> Name -> Bool
+nameIsLocalOrFrom from name
+  | Just mod <- nameModule_maybe name = is_from from mod
+  | otherwise                         = True
+
+nameIsExternalOrFrom :: Module -> Name -> Bool
+nameIsExternalOrFrom from name
+  | Just mod <- nameModule_maybe name = is_from from mod
+  | otherwise                         = False
+
+nameIsHomePackage :: Module -> Name -> Bool
+nameIsHomePackage this_mod
+  = \nm -> case n_sort nm of
+              External nm_mod    -> moduleUnit nm_mod == this_pkg
+              WiredIn nm_mod _ _ -> moduleUnit nm_mod == this_pkg
+              Internal -> True
+              System   -> False
+  where
+    this_pkg = moduleUnit this_mod  
+
+nameIsHomePackageImport :: Module -> Name -> Bool
+nameIsHomePackageImport this_mod
+  = \nm -> case nameModule_maybe nm of
+              Nothing -> False
+              Just nm_mod -> nm_mod /= this_mod
+                          && moduleUnit nm_mod == this_pkg
+  where
+    this_pkg = moduleUnit this_mod
+
+nameIsFromExternalPackage :: HomeUnit -> Name -> Bool
+nameIsFromExternalPackage home_unit name
+  | Just mod <- nameModule_maybe name
+  , notHomeModule home_unit mod
+  = True
+  | otherwise
+  = False
+
+isTyVarName :: Name -> Bool
+isTyVarName name = isTvOcc (nameOccName name)
+
+isKdVarName :: Name -> Bool
+isKdVarName name = isKvOcc (nameOccName name)
+
+isTyConName :: Name -> Bool
+isTyConName name = isTcOcc (nameOccName name)
+
+-- isDataConName :: Name -> Bool
+-- isDataConName name = isDataOcc (nameOccName name)
+
+isValName :: Name -> Bool
+isValName name = isValOcc (nameOccName name)
+
+isVarName :: Name -> Bool
+isVarName = isVarOcc . nameOccName
+
+mkInternalName :: Unique -> OccName -> SrcSpan -> Name
+mkInternalName uniq occ loc = Name { n_uniq = uniq
+                                   , n_sort = Internal
+                                   , n_occ = occ
+                                   , n_loc = loc }
+
+mkClonedInternalName :: Unique -> Name -> Name
+mkClonedInternalName uniq (Name { n_occ = occ, n_loc = loc })
+  = Name { n_uniq = uniq, n_sort = Internal
+         , n_occ = occ, n_loc = loc }
+
+mkDerivedInternalName :: (OccName -> OccName) -> Unique -> Name -> Name
+mkDerivedInternalName derive_occ uniq (Name { n_occ = occ, n_loc = loc })
+  = Name { n_uniq = uniq, n_sort = Internal
+         , n_occ = derive_occ occ, n_loc = loc }
+
+mkExternalName :: Unique -> Module -> OccName -> SrcSpan -> Name
+{-# INLINE mkExternalName #-}
+mkExternalName uniq mod occ loc
+  = Name { n_uniq = uniq, n_sort = External mod,
+           n_occ = occ, n_loc = loc }
+
+mkWiredInName :: Module -> OccName -> Unique -> TyThing -> BuiltInSyntax -> Name
+{-# INLINE mkWiredInName #-}
+mkWiredInName mod occ uniq thing built_in
+  = Name { n_uniq = uniq,
+           n_sort = WiredIn mod thing built_in,
+           n_occ = occ, n_loc = wiredInSrcSpan }
+
+mkSystemName :: Unique -> OccName -> Name
+mkSystemName uniq occ = mkSystemNameAt uniq occ noSrcSpan
+
+mkSystemNameAt :: Unique -> OccName -> SrcSpan -> Name
+mkSystemNameAt uniq occ loc = Name { n_uniq = uniq, n_sort = System
+                                   , n_occ = occ, n_loc = loc }
+
+mkSystemVarName :: Unique -> FastString -> Name
+mkSystemVarName uniq fs = mkSystemName uniq (mkVarOccFS fs)
+
+mkSysTvName :: Unique -> FastString -> Name
+mkSysTvName uniq fs = mkSystemName uniq (mkTyVarOccFS fs)
+
+mkFCallName :: Unique -> FastString -> Name
+mkFCallName uniq str = mkInternalName uniq (mkVarOccFS str) noSrcSpan
+
+setNameUnique :: Name -> Unique -> Name
+setNameUnique name uniq = name {n_uniq = uniq}
+
+
+setNameLoc :: Name -> SrcSpan -> Name
+setNameLoc name loc = name {n_loc = loc}
+
+tidyNameOcc :: Name -> OccName -> Name
+tidyNameOcc name@(Name { n_sort = System }) occ = name { n_occ = occ, n_sort = Internal}
+tidyNameOcc name                            occ = name { n_occ = occ }
+
+localiseName :: Name -> Name
+localiseName n = n { n_sort = Internal }
 
 cmpName :: Name -> Name -> Ordering
 cmpName n1 n2 = n_uniq n1 `nonDetCmpUnique` n_uniq n2
+
+stableNameCmp :: Name -> Name -> Ordering
+stableNameCmp (Name { n_sort = s1, n_occ = occ1 })
+              (Name { n_sort = s2, n_occ = occ2 })
+  = sort_cmp s1 s2 S.<> compare occ1 occ2
+    -- The ordinary compare on OccNames is lexicographic
+  where
+    -- Later constructors are bigger
+    sort_cmp (External m1) (External m2)       = m1 `stableModuleCmp` m2
+    sort_cmp (External {}) _                   = LT
+    sort_cmp (WiredIn {}) (External {})        = GT
+    sort_cmp (WiredIn m1 _ _) (WiredIn m2 _ _) = m1 `stableModuleCmp` m2
+    sort_cmp (WiredIn {})     _                = LT
+    sort_cmp Internal         (External {})    = GT
+    sort_cmp Internal         (WiredIn {})     = GT
+    sort_cmp Internal         Internal         = EQ
+    sort_cmp Internal         System           = LT
+    sort_cmp System           System           = EQ
+    sort_cmp System           _                = GT
 
 instance Eq Name where
   a == b = case (a `compare` b) of
@@ -88,8 +327,30 @@ instance Ord Name where
 instance Uniquable Name where
   getUnique = nameUnique
 
+instance NamedThing Name where
+  getName n = n
+
+instance Data Name where
+  toConstr _ = abstractConstr "Name"
+  gunfold _ _ = error "gunfold"
+  dataTypeOf _ = mkNoRepType "Name"
+
+instance Binary Name where
+  put_ bh name =
+    case getUserData bh of
+      UserData { ud_put_nonbinding_name = put_name } -> put_name bh name
+
+  get bh =
+    case getUserData bh of
+      UserData { ud_get_name = get_name } -> get_name bh
+
 instance Outputable Name where
   ppr name = pprName name
+
+instance OutputableBndr Name where
+  pprBndr _ name = pprName name
+  pprInfixOcc  = pprInfixName
+  pprPrefixOcc = pprPrefixName
 
 pprName :: IsLine doc => Name -> doc
 pprName name@(Name {n_sort = sort, n_uniq = uniq, n_occ = occ})
@@ -115,6 +376,26 @@ pprName name@(Name {n_sort = sort, n_uniq = uniq, n_occ = occ})
     handlePuns _ _ r = r
 {-# SPECIALIZE pprName :: Name -> SDoc #-}
 {-# SPECIALIZE pprName :: Name -> HLine #-}
+
+pprFullName :: Module -> Name -> SDoc
+pprFullName this_mod Name{n_sort = sort, n_uniq = uniq, n_occ = occ} =
+  let mod = case sort of
+        WiredIn  m _ _ -> m
+        External m     -> m
+        System         -> this_mod
+        Internal       -> this_mod
+      in ftext (unitIdFS (moduleUnitId mod))
+         <> colon    <> ftext (moduleNameFS $ moduleName mod)
+         <> dot      <> ftext (occNameFS occ)
+         <> char '_' <> pprUniqueAlways uniq
+
+pprTickyName :: Module -> Name -> SDoc
+pprTickyName this_mod name
+  | isInternalName name = pprName name <+> parens (ppr this_mod)
+  | otherwise           = pprName name
+
+pprNameUnqualified :: Name -> SDoc
+pprNameUnqualified Name { n_occ = occ } = ppr_occ_name occ
 
 pprExternal
   :: Bool
@@ -177,3 +458,53 @@ ppr_underscore_unique uniq
 
 ppr_occ_name :: OccName -> SDoc
 ppr_occ_name occ = ftext (occNameFS occ)
+
+pprDefinedAt :: Name -> SDoc
+pprDefinedAt name = text "Defined" <+> pprNameDefnLoc name
+
+pprNameDefnLoc :: Name -> SDoc
+pprNameDefnLoc name
+  = case nameSrcLoc name of
+       RealSrcLoc s _ -> text "at" <+> ppr s
+       UnhelpfulLoc s
+         | isInternalName name || isSystemName name
+         -> text "at" <+> ftext s
+         | otherwise
+         -> text "in" <+> quotes (ppr (nameModule name))
+
+nameStableString :: Name -> String
+nameStableString Name{..} =
+  nameSortStableString n_sort ++ "$" ++ occNameString n_occ
+
+nameSortStableString :: NameSort -> String
+nameSortStableString System = "$_sys"
+nameSortStableString Internal = "$_in"
+nameSortStableString (External mod) = moduleStableString mod
+nameSortStableString (WiredIn mod _ _) = moduleStableString mod
+
+class NamedThing a where
+    getOccName :: a -> OccName
+    getName    :: a -> Name
+
+    getOccName n = nameOccName (getName n)
+
+instance NamedThing e => NamedThing (Located e) where
+    getName = getName . unLoc
+
+getSrcLoc           :: NamedThing a => a -> SrcLoc
+getSrcSpan          :: NamedThing a => a -> SrcSpan
+getOccString        :: NamedThing a => a -> String
+getOccFS            :: NamedThing a => a -> FastString
+
+getSrcLoc           = nameSrcLoc           . getName
+getSrcSpan          = nameSrcSpan          . getName
+getOccString        = occNameString        . getOccName
+getOccFS            = occNameFS            . getOccName
+
+pprInfixName :: (Outputable a, NamedThing a) => a -> SDoc
+pprInfixName  n = pprInfixVar (isSymOcc (getOccName n)) (ppr n)
+
+pprPrefixName :: NamedThing a => a -> SDoc
+pprPrefixName thing = pprPrefixVar (isSymOcc (nameOccName name)) (ppr name)
+ where
+   name = getName thing
