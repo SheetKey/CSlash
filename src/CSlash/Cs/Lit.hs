fbbe74ad2be0c647c02985e11674cc61b7d0310c
Will <wveatchw@gmail.com>
2024-06-30 19:15:41 -0400

    lit

--- src/CSlash/Cs/Lit.hs
+++ src/CSlash/Cs/Lit.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module CSlash.Cs.Lit where
 
-import CSlash.Syntax.Language.Lit
-
+import CSlash.Language.Syntax.Lit
+import CSlash.Language.Syntax.Extension
+import CSlash.Cs.Extension
+import CSlash.Types.SourceText
 import CSlash.Types.Basic
+import CSlash.Utils.Outputable
+
+type instance XCsChar (CsPass _) = SourceText
+type instance XCsString (CsPass _) = SourceText
 
 csLitNeedsParens :: PprPrec -> CsLit x -> Bool
 csLitNeedsParens p = go
@@ -11,3 +20,9 @@ csLitNeedsParens p = go
     go (CsString{}) = False
     go (CsInt _ x) = p > topPrec && il_neg x
     go (CsDouble{}) = False
+
+instance Outputable (CsLit (CsPass p)) where
+  ppr (CsChar st c) = pprWithSourceText st (pprCsChar c)
+  ppr (CsString st s) = pprWithSourceText st (pprCsString s)
+  ppr (CsInt _ i) = pprWithSourceText (il_text i) (integer (il_value i))
+  ppr (CsDouble _ d) = ppr d
