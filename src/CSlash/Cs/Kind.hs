2fa92d4bfc97751c4452d53ce44d940a4ec6d947
Will <wveatchw@gmail.com>
2024-06-30 19:15:38 -0400

    kind

--- src/CSlash/Cs/Kind.hs
+++ src/CSlash/Cs/Kind.hs
@@ -1,8 +1,16 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module CSlash.Cs.Kind where
 
 import CSlash.Language.Syntax.Extension
 import CSlash.Language.Syntax.Kind
 import CSlash.Cs.Extension
+import CSlash.Types.SrcLoc
+import CSlash.Parser.Annotation
+import CSlash.Utils.Outputable
 
 instance (OutputableBndrId p) => Outputable (CsKind (CsPass p)) where
   ppr kind = pprCsKind kind
@@ -13,12 +21,12 @@ pprCsKind kind = ppr_kind kind
 ppr_lkind :: (OutputableBndrId p) => LCsKind (CsPass p) -> SDoc
 ppr_lkind kind = ppr_kind (unLoc kind)
 
-ppr_kind :: (OutputableBndrId p) => CsType (CsPass p) -> SDoc
-ppr_kind (CsUkd _) = uKindLit
+ppr_kind :: (OutputableBndrId p) => CsKind (CsPass p) -> SDoc
+ppr_kind (CsUKd _) = uKindLit
 ppr_kind (CsAKd _) = aKindLit
 ppr_kind (CsLKd _) = lKindLit
-ppr_kind (CsKdVar _ (L _ name)) = pprOccWithTick Prefix name
-ppr_kind (CsFunKind _ kd1 kd2)
+ppr_kind (CsKdVar _ (L _ name)) = pprPrefixOcc name
+ppr_kind (CsFunKd _ kd1 kd2)
   = sep [ppr_lkind kd1, arrow <+> ppr_lkind kd2]
 ppr_kind (CsQualKd{ csk_ctxt = ctxt, csk_body = kind })
   = sep [pprLCsContextAlways ctxt, ppr_lkind kind]
@@ -30,7 +38,7 @@ pprLCsContextAlways
 pprLCsContextAlways (L _ ctxt)
   = case ctxt of
       [] -> parens empty <+> darrow
-      [L _ kind] -> ppr_kind kind <+> darrow
+      [L _ rel] -> ppr_kdrel rel <+> darrow
       _ -> parens (interpp'SP ctxt) <+> darrow
 
 instance (OutputableBndrId p) => Outputable (CsKdRel (CsPass p)) where
@@ -38,21 +46,25 @@ instance (OutputableBndrId p) => Outputable (CsKdRel (CsPass p)) where
 
 pprCsKdRel
   :: (OutputableBndrId p)
-  => Outputable (CsKdRel (CsPass p))
+  => CsKdRel (CsPass p)
   -> SDoc
 pprCsKdRel rel = ppr_kdrel rel
 
 ppr_lkdrel
   :: (OutputableBndrId p)
-  => Outputable (LCsKdRel (CsPass p))
+  => LCsKdRel (CsPass p)
   -> SDoc
 ppr_lkdrel lkdrel = ppr_kdrel (unLoc lkdrel)
 
 ppr_kdrel
   :: (OutputableBndrId p)
-  -> Outputable (CsKdRel (CsPass p))
+  => CsKdRel (CsPass p)
   -> SDoc
 ppr_kdrel (CsKdLT _ kd1 kd2) 
   = sep [ppr_lkind kd1, char '<', ppr_lkind kd2]
 ppr_kdrel (CsKdLTEQ _ kd1 kd2)
   = sep [ppr_lkind kd1, text "<=", ppr_lkind kd2]
+
+type instance Anno (CsKind (CsPass p)) = SrcSpanAnnA
+
+type instance Anno (CsKdRel (CsPass p)) = SrcSpanAnnA
