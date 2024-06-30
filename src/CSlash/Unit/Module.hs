d5dfebe8b792f91dbbe3ceb82360b8ee358df915
Will <wveatchw@gmail.com>
2024-06-30 19:17:58 -0400

    unit.module

--- src/CSlash/Unit/Module.hs
+++ src/CSlash/Unit/Module.hs
@@ -1,6 +1,26 @@
-module CSlash.Unit.Module where
+{-# LANGUAGE RecordWildCards #-}
+
+module CSlash.Unit.Module
+  ( module CSlash.Unit.Types
+  , module CSlash.Language.Syntax.Module.Name
+  , module CSlash.Unit.Module.Env
+
+  , isHoleModule
+  , stableModuleCmp
+  , moduleStableString
+  ) where
 
 import CSlash.Unit.Types
+import CSlash.Language.Syntax.Module.Name
+import CSlash.Unit.Module.Env
+
+moduleStableString :: Module -> String
+moduleStableString Module{..} =
+  "$" ++ unitString moduleUnit ++ "$" ++ moduleNameString moduleName
+
+stableModuleCmp :: Module -> Module -> Ordering
+stableModuleCmp (Module p1 n1) (Module p2 n2) =
+  stableUnitCmp p1 p2 <> stableModuleNameCmp n1 n2
 
 isHoleModule :: GenModule (GenUnit u) -> Bool
 isHoleModule (Module HoleUnit _) = True
