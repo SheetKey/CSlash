6fd68281b213382dac71cdc55449d107e715fb8d
Will <wveatchw@gmail.com>
2024-06-30 19:16:03 -0400

    imoprts exports

--- src/CSlash/Language/Syntax.hs
+++ src/CSlash/Language/Syntax.hs
@@ -1,11 +1,25 @@
-module CSlash.Language.Syntax (
-  ModuleName(..), CsModule(..)
+module CSlash.Language.Syntax
+  ( module CSlash.Language.Syntax.Binds
+  , module CSlash.Language.Syntax.Decls
+  , module CSlash.Language.Syntax.Expr
+  , module CSlash.Language.Syntax.ImpExp
+  , module CSlash.Language.Syntax.Lit
+  , module CSlash.Language.Syntax.Module.Name
+  , module CSlash.Language.Syntax.Pat
+  , module CSlash.Language.Syntax.Type
+  , module CSlash.Language.Syntax.Extension
+  , ModuleName(..), CsModule(..)
   ) where
 
-import CSlash.Language.Syntax.Extension
-import CSlash.Language.Syntax.Module.Name
-import CSlash.Language.Syntax.ImpExp
+import CSlash.Language.Syntax.Binds
 import CSlash.Language.Syntax.Decls
+import CSlash.Language.Syntax.Expr
+import CSlash.Language.Syntax.ImpExp
+import CSlash.Language.Syntax.Lit
+import CSlash.Language.Syntax.Module.Name
+import CSlash.Language.Syntax.Pat
+import CSlash.Language.Syntax.Type
+import CSlash.Language.Syntax.Extension
 
 -- | Top-level structure of a module.
 data CsModule p = CsModule
