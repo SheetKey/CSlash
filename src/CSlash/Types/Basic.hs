f40d5aeeb2d384cbbd3172f7a082c4737d954893
Will <wveatchw@gmail.com>
2024-07-01 12:22:03 -0400

    basic

--- src/CSlash/Types/Basic.hs
+++ src/CSlash/Types/Basic.hs
@@ -1,22 +1,45 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module CSlash.Types.Basic
-  ( module X
+  ( ConTag
+
   , pprAlternative
+
+  , TyConFlavor(..)
+
+  , module X
   ) where
 
 import GHC.Types.Basic as X hiding
-  (pprAlternative)
-
+  ( TyConFlavour(..)
+  , ConTag
+  , pprAlternative
+  )
+  
+import CSlash.Language.Syntax.Basic
 import CSlash.Utils.Outputable
+
+import Control.DeepSeq
+import Data.Data
 
 pprAlternative :: (a -> SDoc) -> a -> ConTag -> Arity -> SDoc
 pprAlternative pp x alt arity
   = fsep (replicate (alt - 1) vbar ++ [pp x] ++ replicate (arity - alt) vbar)
 
-instance Outputable (TyConFlavour tc) where
+data TyConFlavor tc
+  = TupleFlavor
+  | SumFlavor
+  | TypeFunFlavor
+  deriving (Eq, Data)
+
+instance Outputable (TyConFlavor tc) where
   ppr = text . go
     where
-      go (ClassFlavour) = "class"
-      go (TupleFlavour _) = "tuple"
-      go SumFlavour = "sum"
-      go TypeSynonymFlavour = "type synonym"
-      go _ = error "This 'TyConFlavour' should not be used yet"
+      go TupleFlavor = "tuple"
+      go SumFlavor = "sum"
+      go TypeFunFlavor = "type synonym"
+
+instance NFData tc => NFData (TyConFlavor tc) where
+  rnf TupleFlavor = ()
+  rnf SumFlavor = ()
+  rnf TypeFunFlavor = ()
