473c7f024715b1d9b6417d6ea088d55d3c454bf9
Will <wveatchw@gmail.com>
2024-07-02 11:55:24 -0400

    var

--- src/CSlash/Types/Var.hs
+++ src/CSlash/Types/Var.hs
@@ -1,3 +1,16 @@
 module CSlash.Types.Var where
 
+import CSlash.Utils.Misc
+
+import Data.Data
+
+type Id = Var
+
 data Var
+
+instance Eq Var
+
+instance Data Var where
+  toConstr _ = abstractConstr "Var"
+  gunfold _ _ = error "gunfold"
+  dataTypeOf _ = mkNoRepType "Var"
