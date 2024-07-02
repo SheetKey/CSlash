251b7e013ef2c26778a8b8a3ce1782acb3ccd026
Will <wveatchw@gmail.com>
2024-07-02 11:54:15 -0400

    decls

--- src/CSlash/Cs/Decls.hs
+++ src/CSlash/Cs/Decls.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module CSlash.Cs.Decls
-  (
+  ( CsDecl(..), LCsDecl(..)
   ) where
 
 import CSlash.Language.Syntax.Decls
 
 import CSlash.Language.Syntax.Extension
 import CSlash.Cs.Extension
+import CSlash.Cs.Binds
 import CSlash.Parser.Annotation
 import CSlash.Utils.Outputable
+
+type instance XValD (CsPass _) = NoExtField
+type instance XSigD (CsPass _) = NoExtField
 
 instance (OutputableBndrId p) => Outputable (CsDecl (CsPass p)) where
   ppr (ValD _ bind) = ppr bind
