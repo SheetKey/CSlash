53a97a44cb8537e5cc50f39e9cac3cfd4bdd2132
Will <wveatchw@gmail.com>
2024-07-02 11:55:16 -0400

    fixity

--- src/CSlash/Types/Fixity.hs
+++ src/CSlash/Types/Fixity.hs
@@ -1,3 +1,13 @@
-module CSlash.Types.Fixity (module X) where
+module CSlash.Types.Fixity ( module X ) where
 
 import GHC.Types.Fixity as X
+
+import CSlash.Utils.Outputable
+
+instance Outputable Fixity where
+  ppr (Fixity _ prec dir) = hcat [ppr dir, space, int prec]
+
+instance Outputable FixityDirection where
+  ppr InfixL = text "infixl"
+  ppr InfixR = text "infixr"
+  ppr InfixN = text "infix"
