36599439ea8c7fa2d3635b000ed6ba3bdfc42542
Will <wveatchw@gmail.com>
2024-07-15 14:44:19 -0400

    core.type.ppr

---
+++ src/CSlash/Core/Type/Ppr.hs
@@ -0,0 +1,35 @@
+module CSlash.Core.Type.Ppr where
+
+import CSlash.Core.Type.Rep
+import CSlash.Types.Var
+-- import CSlash.Iface.Type
+
+import CSlash.Utils.Outputable
+import CSlash.Utils.Misc
+import CSlash.Type.Basic
+
+
+pprType :: Type -> SDoc
+pprType = undefined -- pprPrecType topPrec
+
+-- pprPrecType :: PprPrec -> Type -> SDoc
+-- pprPreType = pprPrecTypeX emptyTidyEnv
+
+-- pprPrecTypeX :: TidyEnv -> PprPrec -> Type -> SDoc
+-- pprPrecTypeX env prec ty
+--   = getPprStyle $ \ sty ->
+--     getPprDebug $ \ debug ->
+--                     if debug
+--                     then debug_ppr_ty prec ty
+--                     else pprPrecIfaceType prec (tidyToIfaceTypeStyX env ty sty)
+
+-- tidyToIfaceTypeStyX :: TidyEnv -> Type -> PprStyle -> IfaceType
+-- tidyToIfaceTypeStyX env ty sty
+--   | useStyle sty = tidyToIfaceTypeX env ty
+--   | otherwise = toIfaceTypeX (tyVarsOfType ty) ty
+
+-- tidyToIfaceTypeX :: TidyEnv -> Type -> IfaceType
+-- tidyToIfaceTypeX env ty = toIfaceTypeX (mkVarSet free_tvs) (tidyType env' ty)
+--   where
+--     env' = tidyFreeTyVars env free_tvs
+--     free_tvs = tyVarsOfTypeWellScoped ty
---
+++ src/CSlash/Core/Type/Ppr.hs-boot
@@ -0,0 +1,6 @@
+module CSlash.Core.Type.Ppr where
+
+import {-# SOURCE #-} CSlash.Core.Type.Rep (Type)
+import CSlash.Utils.Outputalbe (SDoc)
+
+pprType :: Type -> SDoc
