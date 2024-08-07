1b113e02e97cdb33a7a78464208dfed4b1f5bcb0
Will <wveatchw@gmail.com>
2024-08-07 11:24:56 -0400

    add prims and helpers

--- src/CSlash/Builtin/Types/Prim.hs
+++ src/CSlash/Builtin/Types/Prim.hs
@@ -1,10 +1,16 @@
 module CSlash.Builtin.Types.Prim where
 
+import {-# SOURCE #-} CSlash.Types.TyThing (mkATyCon)
+
 import CSlash.Core.TyCon
-import CSlash.Core.TyCo.Rep
+import CSlash.Core.Type.Rep
+import CSlash.Core.Kind
 
 import CSlash.Types.Var
-  ( TyVarBinder, TyVar, binderVar, binderVars, mkTyVar, mkTyVarBinder, mkTyVarBinders )
+  ( TyVarBinder, TypeVar, KindVar
+  , binderVar, binderVars
+  , mkTyVar, mkKdVar
+  , mkTyVarBinder, mkTyVarBinders )
 import CSlash.Types.Name
 import CSlash.Types.SrcLoc
 import CSlash.Types.Unique
@@ -17,6 +23,23 @@ import CSlash.Utils.Outputable
 
 import CSlash.Data.FastString
 import Data.Char
+
+{- *********************************************************************
+*                                                                      *
+             Building blocks
+*                                                                      *
+********************************************************************* -}
+
+mkPrimTc :: FastString -> Unique -> TyCon -> Name
+mkPrimTc = mkGenPrimTc UserSyntax
+
+mkGenPrimTc :: BuiltInSyntax -> FastString -> Unique -> TyCon -> Name
+mkGenPrimTc built_in_syntax occ key tycon
+  = mkWiredInName cSLASH_PRIM
+                  (mkTcOccFS occ)
+                  key
+                  (mkATyCon tycon)
+                  built_in_syntax
 
 {- *********************************************************************
 *                                                                      *
@@ -35,16 +58,16 @@ mkTemplateKindVars i
     , mkKdVar (mk_kv_name i ('k' : show i)) )
 
 mk_kv_name :: Int -> String -> Name
-mk_kv_name u s = mkInternalName (mkAlphasTyVarUnique u)
+mk_kv_name u s = mkInternalName (mkAlphaTyVarUnique u)
                                 (mkTyVarOccFS (mkFastString s))
                                 noSrcSpan
 
 mk_tv_name :: Int -> String -> Name
-mk_tv_name u s = mkInternalName (mkAlphasTyVarUnique u)
+mk_tv_name u s = mkInternalName (mkAlphaTyVarUnique u)
                                 (mkTyVarOccFS (mkFastString s))
                                 noSrcSpan
 
-mkTemplateTyVarsFrom :: Int -> [Kind] -> [TyVar]
+mkTemplateTyVarsFrom :: Int -> [Kind] -> [TypeVar]
 mkTemplateTyVarsFrom i kinds
   = [ mkTyVar name kind
     | (kind, index) <- zip kinds [0..(i-1)]
@@ -57,3 +80,46 @@ mkTemplateTyVarsFrom i kinds
 mkTemplateTyConBindersFrom :: Int -> [Kind] -> [TyConBinder]
 mkTemplateTyConBindersFrom i kinds
   = mkSpecifiedTyConBinders (mkTemplateTyVarsFrom i kinds)
+
+mkTemplateTyConBinders :: [Kind] -> [TyConBinder]
+mkTemplateTyConBinders kds = mkTemplateTyConBindersFrom (length kds) kds
+
+mkTemplateTyConBindersKindRel :: (Kind -> Kind -> KdRel) -> Int -> ([TyConBinder], Kind)
+mkTemplateTyConBindersKindRel rel arity
+  = let (kind_vars, res_kind_var) = mkTemplateKindVars arity
+        kinds = KdVarKd <$> kind_vars
+        res_kind = KdVarKd res_kind_var
+        tc_binders = mkTemplateTyConBinders kinds
+        tc_kind_constrs = KdContext $ (`rel` res_kind) <$> kinds
+        tc_kind = FunKd FKF_C_K tc_kind_constrs $ foldr (FunKd FKF_K_K) res_kind kinds
+    in (tc_binders, tc_kind)
+
+mkTemplateTyConBindersKindLTEQ :: Int -> ([TyConBinder], Kind)
+mkTemplateTyConBindersKindLTEQ = mkTemplateTyConBindersKindRel LTEQKd
+
+{- *********************************************************************
+*                                                                      *
+                FunTyCon
+*                                                                      *
+********************************************************************* -}
+
+{-
+Unlike GHC, we have a single function tycon "FUN" that has a kind.
+Its kind may be UKd, AKd, LKd, or a kdvarkd.
+For kind polymorphism, which we want, we have
+  FUN : k1 <= k3, k2 <= k3 => k1 -> k2 -> k3
+-}
+
+fUNTyConName :: Name
+fUNTyConName = mkPrimTc (fsLit "FUN") fUNTyConKey fUNTyCon
+
+fUNTyCon :: TyCon
+fUNTyCon = mkPrimTyCon fUNTyConName tc_bndrs tc_kind
+  where
+    -- (kind_vars, res_kind_var) = mkTemplateKindVars 2
+    -- kinds = KdVarKd <$> kind_vars
+    -- res_kind = KdVarKd res_kind_var
+    -- tc_bndrs = mkTemplateTyConBinders kindvars
+    -- tc_kind_constrs = KdContext $ (`LTEQKd` res_kind) <$> kinds
+    -- tc_kind = FunKd FKF_C_K tc_kind_constrs $ foldr (FunKd FKF_K_K) res_kind kinds
+    (tc_bndrs, tc_kind) = mkTemplateTyConBindersKindLTEQ 2
