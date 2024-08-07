7716d5cafda0eb90fe541b4aa10909fba62489a3
Will <wveatchw@gmail.com>
2024-08-07 11:25:12 -0400

    add builtin names

--- src/CSlash/Builtin/Names.hs
+++ src/CSlash/Builtin/Names.hs
@@ -3,9 +3,24 @@ module CSlash.Builtin.Names
     module CSlash.Builtin.Names
   ) where
 
+import CSlash.Unit.Types
+import CSlash.Types.Name.Occurrence
+import CSlash.Types.Name.Reader
 import CSlash.Types.Unique
+import CSlash.Builtin.Uniques
+import CSlash.Types.Name
+import CSlash.Types.SrcLoc
+import CSlash.Data.FastString
 import CSlash.Data.List.Infinite (Infinite (..))
 import qualified CSlash.Data.List.Infinite as Inf
+
+import CSlash.Language.Syntax.Module.Name
+
+{- *********************************************************************
+*                                                                      *
+     allNameStrings
+*                                                                      *
+********************************************************************* -}
 
 allNameStrings :: Infinite String
 -- Infinite list of a,b,c...z, aa, ab, ac, ... etc
@@ -14,3 +29,42 @@ allNameStrings = Inf.allListsOf ['a'..'z']
 allNameStringList :: [String]
 -- Infinite list of a,b,c...z, aa, ab, ac, ... etc
 allNameStringList = Inf.toList allNameStrings
+
+{- *********************************************************************
+*                                                                      *
+      Module names
+*                                                                      *
+********************************************************************* -}
+
+cSLASH_PRIM :: Module
+cSLASH_PRIM = mkPrimModule (fsLit "CSlash.Prim")
+
+cSLASH_TYPES :: Module
+cSLASH_TYPES = mkPrimModule (fsLit "CSlash.Types")
+
+mkPrimModule :: FastString -> Module
+mkPrimModule m = mkModule primUnit (mkModuleNameFS m)
+
+{- *********************************************************************
+*                                                                      *
+               Uniques for wired-in TyCons
+*                                                                      *
+********************************************************************* -}
+
+boolTyConKey :: Unique
+boolTyConKey = mkWiredInTyConUnique 4
+
+fUNTyConKey :: Unique
+fUNTyConKey = mkWiredInTyConUnique 13
+
+{- *********************************************************************
+*                                                                      *
+               Uniques for wired-in DataCons
+*                                                                      *
+********************************************************************* -}
+
+falseDataConKey :: Unique
+falseDataConKey = mkWiredInDataConUnique 4
+
+trueDataConKey :: Unique
+trueDataConKey = mkWiredInDataConUnique 14
