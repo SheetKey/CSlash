5afc17fb941ce882e21cb7f9f3c8631fa3f18f26
Will <wveatchw@gmail.com>
2024-08-07 11:09:42 -0400

    varset helpers

--- src/CSlash/Types/Var/Set.hs
+++ src/CSlash/Types/Var/Set.hs
@@ -1,6 +1,6 @@
 module CSlash.Types.Var.Set where
 
-import CSlash.Types.Var ( Var, TyVar, CoVar, TyCoVar, Id )
+import CSlash.Types.Var ( Var, TypeVar, KindVar, Id )
 import CSlash.Types.Unique
 import CSlash.Types.Name ( Name )
 import CSlash.Types.Unique.Set
@@ -15,5 +15,25 @@ type IdSet = UniqSet Id
 
 type TyVarSet = UniqSet TypeVar
 
+type KdVarSet = UniqSet KindVar
+
 emptyVarSet :: VarSet
 emptyVarSet = emptyUniqSet
+
+extendVarSet :: VarSet -> Var -> VarSet
+extendVarSet = addOneToUniqSet
+
+elemVarSet :: Var -> VarSet -> Bool
+elemVarSet = elementOfUniqSet
+
+minusVarSet :: VarSet -> VarSet -> VarSet
+minusVarSet = minusUniqSet
+
+isEmptyVarSet :: VarSet -> Bool
+isEmptyVarSet = isEmptyUniqSet
+
+subVarSet :: VarSet -> VarSet -> Bool
+subVarSet s1 s2 = isEmptyVarSet (s1 `minusVarSet` s2)
+
+pprVarSet :: VarSet -> ([Var] -> SDoc) -> SDoc
+pprVarSet = pprUFM . getUniqSet
