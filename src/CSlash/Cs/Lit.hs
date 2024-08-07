643c5890da622ef08d9f051da15b58e24284314a
Will <wveatchw@gmail.com>
2024-08-07 11:21:32 -0400

    fixes

--- src/CSlash/Cs/Lit.hs
+++ src/CSlash/Cs/Lit.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 
 module CSlash.Cs.Lit
@@ -6,17 +9,34 @@ module CSlash.Cs.Lit
   , module CSlash.Cs.Lit
   ) where
 
-import CSlash.Language.Syntax.Lit
-import CSlash.Language.Syntax.Extension
-import CSlash.Cs.Extension
-import CSlash.Types.SourceText
 import CSlash.Types.Basic
+import CSlash.Types.SourceText
+import CSlash.Core.Type
 import CSlash.Utils.Outputable
+import CSlash.Cs.Extension
+import CSlash.Language.Syntax.Expr (CsExpr)
+import CSlash.Language.Syntax.Extension
+import CSlash.Language.Syntax.Lit
 
 type instance XCsChar (CsPass _) = SourceText
 type instance XCsString (CsPass _) = SourceText
 type instance XCsInt (CsPass _) = NoExtField
 type instance XCsDouble (CsPass _) = NoExtField
+
+data OverLitRn = OverLitRn
+  { ol_rebindable :: Bool
+  , ol_from_fun :: LIdP Rn
+  }
+
+data OverLitTc = OverLitTc
+  { ol_rebindable :: Bool
+  , ol_witness :: CsExpr Tc
+  , ol_type :: Type
+  }
+
+type instance XOverLit Ps = NoExtField
+type instance XOverLit Rn = OverLitRn
+type instance XOverLit Tc = OverLitTc
 
 csLitNeedsParens :: PprPrec -> CsLit x -> Bool
 csLitNeedsParens p = go
@@ -31,3 +51,6 @@ instance Outputable (CsLit (CsPass p)) where
   ppr (CsString st s) = pprWithSourceText st (pprCsString s)
   ppr (CsInt _ i) = pprWithSourceText (il_text i) (integer (il_value i))
   ppr (CsDouble _ d) = ppr d
+
+instance OutputableBndrId p => Outputable (CsOverLit (CsPass p)) where
+  ppr = undefined
