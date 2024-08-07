c4fe06a695dc690bff6f5a63f47744571a3e54a0
Will <wveatchw@gmail.com>
2024-08-07 11:14:31 -0400

    imports, mkGlobalId

--- src/CSlash/Types/Id.hs
+++ src/CSlash/Types/Id.hs
@@ -1,6 +1,28 @@
 module CSlash.Types.Id
   ( Var, Id
+
+  , mkGlobalId
   ) where
 
-import CSlash.Types.Var (Id)
+import CSlash.Types.Id.Info
+import CSlash.Types.Basic
+
+import CSlash.Types.Var (Var, Id)
 import qualified CSlash.Types.Var as Var
+
+import CSlash.Core.Type
+import CSlash.Types.RepType
+import CSlash.Core.DataCon
+import CSlash.Types.Name
+import CSlash.Unit.Module
+import CSlash.Data.Maybe
+import CSlash.Types.SrcLoc
+import CSlash.Types.Unique
+import CSlash.Data.FastString
+
+import CSlash.Utils.Misc
+import CSlash.Utils.Outputable
+import CSlash.Utils.Panic
+
+mkGlobalId :: IdDetails -> Name -> Type -> IdInfo -> Id
+mkGlobalId = Var.mkGlobalVar
