e92f1bb9b0c8433e7c15938ac63548bf0cf037fd
Will <wveatchw@gmail.com>
2024-08-07 11:13:07 -0400

    literal instances

--- src/CSlash/Types/Literal.hs
+++ src/CSlash/Types/Literal.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module CSlash.Types.Literal where
 
 import CSlash.Utils.Outputable
 
 import Data.Data
+import GHC.Exts (isTrue#, dataToTag#, (<#))
+import Numeric (fromRat)
 
 data Literal
   = LitChar Char
@@ -21,6 +26,23 @@ data LitNumType
 instance Outputable Literal where
   ppr = pprLiteral id
 
+instance Eq Literal where
+  a == b = compare a b == EQ
+
+instance Ord Literal where
+  compare = cmpLit
+
+cmpLit :: Literal -> Literal -> Ordering
+cmpLit (LitChar a) (LitChar b) = a `compare` b
+cmpLit LitNullAddr LitNullAddr = EQ
+cmpLit (LitFloat a) (LitFloat b) = a `compare` b
+cmpLit (LitDouble a) (LitDouble b) = a `compare` b
+cmpLit (LitNumber nt1 a) (LitNumber nt2 b)
+  = (nt1 `compare` nt2) `mappend` (a `compare` b)
+cmpLit lit1 lit2
+  | isTrue# (dataToTag# lit1 <# dataToTag# lit2) = LT
+  | otherwise = GT
+
 pprLiteral :: (SDoc -> SDoc) -> Literal -> SDoc
 pprLiteral _ (LitChar c) = pprCsChar c
 pprLiteral _ LitNullAddr = text "__NULL"
@@ -29,4 +51,4 @@ pprLiteral _ (LitDouble d) = double (fromRat d)
 pprLiteral _ (LitNumber nt i)
   = case nt of
       LitNumInt _ -> integer i
-      LitNummWord _ -> word i
+      LitNumWord _ -> word i
