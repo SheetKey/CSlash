ebf313e01e0babb7f982bc8b987a1026c8d1dd92
Will <wveatchw@gmail.com>
2024-08-07 11:11:45 -0400

    outputable uniqset

--- src/CSlash/Types/Unique/Set.hs
+++ src/CSlash/Types/Unique/Set.hs
@@ -1,3 +1,11 @@
 module CSlash.Types.Unique.Set ( module X ) where
 
-import GHC.Types.Unique.Set as X
+import GHC.Types.Unique.Set as X hiding (pprUniqSet)
+
+import CSlash.Utils.Outputable
+
+instance Outputable a => Outputable (UniqSet a) where
+  ppr = pprUniqSet ppr
+
+pprUniqSet :: (a -> SDoc) -> UniqSet a -> SDoc
+pprUniqSet f = braces . pprWithCommas f . nonDetEltsUniqSet
