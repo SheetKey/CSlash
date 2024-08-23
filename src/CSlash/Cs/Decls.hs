0b087c28bd0715a3973b501f3b6784308f37f1ca
Will <wveatchw@gmail.com>
2024-08-23 16:20:41 -0400

    imports and partitionbindsandsigs

--- src/CSlash/Cs/Decls.hs
+++ src/CSlash/Cs/Decls.hs
@@ -11,20 +11,29 @@ module CSlash.Cs.Decls
 
 import CSlash.Language.Syntax.Decls
 
+import CSlash.Cs.Binds
+import CSlash.Cs.Type
+import CSlash.Types.Basic
 import CSlash.Language.Syntax.Extension
 import CSlash.Cs.Extension
-import CSlash.Cs.Binds
 import CSlash.Parser.Annotation
+import CSlash.Types.Name
+import CSlash.Types.Name.Set
+import CSlash.Types.Fixity
+
+-- others:
 import CSlash.Utils.Outputable
+import CSlash.Utils.Panic
+import CSlash.Types.SrcLoc
+import CSlash.Types.SourceText
+import CSlash.Core.Type
+
+import CSlash.Data.Bag
+import CSlash.Data.Maybe
+import Data.Data (Data)
 
 type instance XValD (CsPass _) = NoExtField
 type instance XSigD (CsPass _) = NoExtField
-
-instance (OutputableBndrId p) => Outputable (CsDecl (CsPass p)) where
-  ppr (ValD _ bind) = ppr bind
-  ppr (SigD _ sd) = ppr sd
-
-type instance Anno (CsDecl (CsPass _)) = SrcSpanAnnA
 
 partitionBindsAndSigs :: [LCsDecl Ps] -> (LCsBinds Ps, [LSig Ps])
 partitionBindsAndSigs [] = (emptyBag, [])
@@ -33,3 +42,9 @@ partitionBindsAndSigs ((L l decl) : ds) =
     case decl of
       ValD _ b -> (L l b `consBag` bs, ss)
       SigD _ s -> (bs, L l s : ss)
+
+instance (OutputableBndrId p) => Outputable (CsDecl (CsPass p)) where
+  ppr (ValD _ bind) = ppr bind
+  ppr (SigD _ sd) = ppr sd
+
+type instance Anno (CsDecl (CsPass _)) = SrcSpanAnnA
