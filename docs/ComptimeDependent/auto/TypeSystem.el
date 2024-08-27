2103a7404922436162f7608c4c400234b584ed39
Will <wveatchw@gmail.com>
2024-08-27 17:53:20 -0400

    docs

--- docs/ComptimeDependent/auto/TypeSystem.el
+++ docs/ComptimeDependent/auto/TypeSystem.el
@@ -3,8 +3,8 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("amsart" "11pt")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "urladdr")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "email")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "urladdr")
    (TeX-run-style-hooks
     "latex2e"
     "amsart"
