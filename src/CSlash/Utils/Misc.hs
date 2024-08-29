303acb18b29a04781de39ac666326cbf52308c2a
Will <wveatchw@gmail.com>
2024-08-29 18:55:50 -0400

    foldr2 util

--- src/CSlash/Utils/Misc.hs
+++ src/CSlash/Utils/Misc.hs
@@ -1,3 +1,16 @@
-module CSlash.Utils.Misc ( module X ) where
+module CSlash.Utils.Misc
+  ( module X
+  , module CSlash.Utils.Misc
+  ) where
 
 import GHC.Utils.Misc as X
+
+import CSlash.Utils.Panic.Plain
+
+-- Combination of foldr and zip. Must be the same length
+foldr2 :: (a -> b -> c -> c) -> c -> [a] -> [b] -> c
+foldr2 f c = go
+  where
+    go [] [] = c
+    go (a:as) (b:bs) = f a b (go as bs)
+    go _ _ = panic "Util: foldr2"
