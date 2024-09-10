84d22f632173be8f2bcba4bf171b76da18a6560b
Will <wveatchw@gmail.com>
2024-09-10 19:01:18 -0400

    driver.env

---
+++ src/CSlash/Driver/Env.hs
@@ -0,0 +1,50 @@
+module CSlash.Driver.Env
+  ( Cs (..)
+  , CsEnv (..)
+  , module CSlash.Driver.Env
+  ) where
+
+import CSlash.Driver.DynFlags
+-- import GHC.Driver.Errors ( printOrThrowDiagnostics )
+import CSlash.Driver.Errors.Types ( CsMessage )
+-- import GHC.Driver.Config.Logger (initLogFlags)
+-- import GHC.Driver.Config.Diagnostic (initDiagOpts, initPrintConfig)
+import CSlash.Driver.Env.Types ( Cs(..), CsEnv(..) )
+
+-- import GHC.Runtime.Context
+-- import GHC.Runtime.Interpreter.Types (Interp)
+
+import CSlash.Unit
+-- import GHC.Unit.Module.ModGuts
+-- import GHC.Unit.Module.ModIface
+-- import GHC.Unit.Module.ModDetails
+-- import GHC.Unit.Home.ModInfo
+-- import GHC.Unit.Env
+-- import GHC.Unit.External
+
+-- import GHC.Core         ( CoreRule )
+-- import GHC.Core.FamInstEnv
+-- import GHC.Core.InstEnv
+
+-- import GHC.Types.Annotations ( Annotation, AnnEnv, mkAnnEnv, plusAnnEnv )
+-- import GHC.Types.CompleteMatch
+import CSlash.Types.Error ( emptyMessages, Messages )
+import CSlash.Types.Name
+import CSlash.Types.Name.Env
+import CSlash.Types.TyThing
+
+import CSlash.Builtin.Names ( cSLASH_PRIM )
+
+import CSlash.Data.Maybe
+
+import CSlash.Utils.Exception as Ex
+import CSlash.Utils.Outputable
+-- import GHC.Utils.Monad
+import CSlash.Utils.Panic
+import CSlash.Utils.Misc
+import CSlash.Utils.Logger
+
+import Data.IORef
+import qualified Data.Set as Set
+import CSlash.Unit.Module.Graph
+
