86d4ac20d66dcad007fb8cce70f6c6afbff61975
Will <wveatchw@gmail.com>
2024-09-10 19:02:50 -0400

    driver.session

---
+++ src/CSlash/Driver/Session.hs
@@ -0,0 +1,13 @@
+module CSlash.Driver.Session
+  ( DumpFlag(..)
+  , GeneralFlag(..)
+  , WarningFlag(..)
+  , dopt
+  , gopt
+  , wopt
+  , DynFlags(..)
+  , HasDynFlags(..), ContainsDynFlags(..)
+  ) where 
+
+import CSlash.Driver.DynFlags
+import CSlash.Driver.Flags
