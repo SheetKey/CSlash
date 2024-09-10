1d0430aabad2b9e551a86b330c8df0e8eafe5f69
Will <wveatchw@gmail.com>
2024-09-10 19:06:58 -0400

    unit.home.modinfo

---
+++ src/CSlash/Unit/Home/ModInfo.hs
@@ -0,0 +1,32 @@
+module CSlash.Unit.Home.ModInfo where
+
+import CSlash.Unit.Module.ModIface
+import CSlash.Unit.Module.ModDetails
+import CSlash.Unit.Module
+
+import CSlash.Linker.Types ( Linkable(..){-, isObjectLinkable-} )
+
+import CSlash.Types.Unique
+import CSlash.Types.Unique.DFM
+
+import CSlash.Utils.Outputable
+import Data.List (sortOn)
+import Data.Ord
+import CSlash.Utils.Panic
+
+data HomeModInfo = HomeModInfo
+  { hm_iface :: !ModIface
+  , hm_details :: ModDetails
+  , hm_linkable :: !HomeModLinkable
+  }
+
+data HomeModLinkable = HomeModLinkable
+  { homeMod_object :: !(Maybe Linkable) }
+
+type HomePackageTable = DModuleNameEnv HomeModInfo
+
+pprHPT :: HomePackageTable -> SDoc
+pprHPT hpt = pprUDFM hpt $ \hms ->
+  vcat [ ppr (mi_module (hm_iface hm))
+       | hm <- hms
+       ]
