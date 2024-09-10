f6f0f8c5a9d8fed5f4ed919f9071fcdda0372e99
Will <wveatchw@gmail.com>
2024-09-10 19:15:10 -0400

    rename

--- src/CSlash/Types/SourceFile.hs
+++ src/CSlash/Types/SourceFile.hs
@@ -3,12 +3,15 @@ module CSlash.Types.SourceFile where
 import CSlash.Utils.Binary
 import CSlash.Unit.Types
 
-data CslSource = CsSrcFile
+data CsSource = CsSrcFile
   deriving (Eq, Ord, Show)
 
-instance Binary CslSource where
+instance Binary CsSource where
   put_ bh CsSrcFile = putByte bh 0
   get bh = do
     h <- getByte bh
     case h of
       _ -> return CsSrcFile
+
+csSourceString :: CsSource -> String
+csSourceString CsSrcFile = ""
