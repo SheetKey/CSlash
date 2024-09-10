4f236a60d58746786c37bfe42327c523d765e516
Will <wveatchw@gmail.com>
2024-09-10 19:15:26 -0400

    ppr

--- src/CSlash/Types/Unique/DFM.hs
+++ src/CSlash/Types/Unique/DFM.hs
@@ -1,3 +1,11 @@
-module CSlash.Types.Unique.DFM ( module X ) where
+module CSlash.Types.Unique.DFM
+  ( module X
+  , module CSlash.Types.Unique.DFM
+  ) where
 
-import GHC.Types.Unique.DFM as X
+import GHC.Types.Unique.DFM as X hiding (pprUDFM)
+
+import CSlash.Utils.Outputable
+
+pprUDFM :: UniqDFM key a -> ([a] -> SDoc) -> SDoc
+pprUDFM ufm pp = pp (eltsUDFM ufm)
