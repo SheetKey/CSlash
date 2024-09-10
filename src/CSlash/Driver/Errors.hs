90642d77723d780e8c3ed9e304ee9d991681681b
Will <wveatchw@gmail.com>
2024-09-10 19:01:58 -0400

    driver.errors

---
+++ src/CSlash/Driver/Errors.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module CSlash.Driver.Errors where
+
+import CSlash.Driver.Errors.Types
+import CSlash.Types.SourceError
+import CSlash.Types.Error
+import CSlash.Utils.Error
+import CSlash.Utils.Outputable (hang, ppr, ($$),  text, mkErrStyle, sdocStyle, updSDocContext )
+import CSlash.Utils.Logger
+
+printMessages
+  :: forall a. Diagnostic a => Logger -> DiagnosticOpts a -> DiagOpts -> Messages a -> IO ()
+printMessages logger msg_opts opts msgs
+  = sequence_ [ let style = mkErrStyle name_ppr_ctx
+                    ctx = (diag_ppr_ctx opts) { sdocStyle = style }
+                in logMsg logger (MCDiagnostic sev reason (diagnosticCode dia)) s $
+                   updSDocContext (const ctx) (messageWithHints dia)
+              | msg@MsgEnvelope { errMsgSpan = s
+                                , errMsgDiagnostic = dia
+                                , errMsgSeverity = sev
+                                , errMsgReason = reason
+                                , errMsgContext = name_ppr_ctx }
+                  <- sortMsgBag (Just opts) (getMessages msgs)
+              ]
+  where
+    messageWithHints :: Diagnostic a => a -> SDoc
+    messageWithHints e =
+      let main_msg = formatBulleted $ diagnosticMessage msg_opts e
+      in case diagnosticHints e of
+        [] -> main_msg
+        [h] -> main_msg $$ hang (text "Suggested fix:") 2 (ppr h)
+        hs -> main_msg $$ hang (text "Suggested fixes:") 2
+                               (formatBulleted $ mkDecorated . map ppr $ hs)
