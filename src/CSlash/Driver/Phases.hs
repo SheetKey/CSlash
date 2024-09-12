98ba0eb960b95db1b9262ded99ab7f5ba785dade
Will <wveatchw@gmail.com>
2024-09-12 18:43:08 -0400

    phase stuff

--- src/CSlash/Driver/Phases.hs
+++ src/CSlash/Driver/Phases.hs
@@ -27,10 +27,13 @@ startPhase :: String -> Phase
 startPhase "csl" = Cs CsSrcFile
 
 csish_src_suffixes :: [String]
-csish_src_suffixes = [ "csl" ]
+csish_src_suffixes = csish_user_src_suffixes
 
 csish_suffixes :: [String]
 csish_suffixes = csish_src_suffixes
+
+csish_user_src_suffixes :: [String]
+csish_user_src_suffixes = [ "csl" ]
 
 isCsishSuffix :: String -> Bool
 isCsishSuffix s = s `elem` csish_suffixes
@@ -38,12 +41,18 @@ isCsishSuffix s = s `elem` csish_suffixes
 isCsSrcSuffix :: String -> Bool
 isCsSrcSuffix s = s `elem` csish_src_suffixes
 
+isCsUserSrcSuffix :: String -> Bool
+isCsUserSrcSuffix s = s `elem` csish_user_src_suffixes
+
 isSourceSuffix :: String -> Bool
 isSourceSuffix suff
   = isCsishSuffix suff
 
 isCsSrcFilename :: FilePath -> Bool
 isCsSrcFilename f = isCsSrcSuffix (drop 1 $ takeExtension f)
+
+isCsUserSrcFilename :: FilePath -> Bool
+isCsUserSrcFilename f = isCsUserSrcSuffix (drop 1 $ takeExtension f)
 
 isSourceFilename :: FilePath -> Bool
 isSourceFilename f = isSourceSuffix (drop 1 $ takeExtension f)
