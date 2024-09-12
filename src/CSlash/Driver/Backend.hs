ad78350a0e31be31c700268de4a31838aed3a296
Will <wveatchw@gmail.com>
2024-09-12 19:57:21 -0400

    backends

--- src/CSlash/Driver/Backend.hs
+++ src/CSlash/Driver/Backend.hs
@@ -1,8 +1,19 @@
 module CSlash.Driver.Backend where
 
 import CSlash.Driver.Backend.Internal
+import CSlash.Driver.Phases
 
 import CSlash.Utils.Error
 import CSlash.Utils.Panic
 
+import CSlash.Platform
+
+platformDefaultBackend :: Platform -> Backend
+platformDefaultBackend platform
+  | platformUnregisterised platform = panic "unregisterised platform"
+  | otherwise = llvmBackend
+
 newtype Backend = Named BackendName
+
+llvmBackend :: Backend
+llvmBackend = Named LLVM
