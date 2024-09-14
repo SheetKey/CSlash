8c9ab0580c50e996a5499733314bb33be96038a9
Will <wveatchw@gmail.com>
2024-09-13 20:17:02 -0400

    globalvars

--- src/CSlash/Utils/GlobalVars.hs
+++ src/CSlash/Utils/GlobalVars.hs
@@ -1,4 +1,31 @@
 module CSlash.Utils.GlobalVars where
 
+import GHC.Conc.Sync ( sharedCAF )
+
+import System.IO.Unsafe
+import Data.IORef
+import Foreign (Ptr)
+
+{-# NOINLINE v_unsafeHasPprDebug #-}
+v_unsafeHasPprDebug :: IORef Bool
+v_unsafeHasPprDebug = global False
+
+{-# NOINLINE v_unsafeHasNoDebugOutput #-}
+v_unsafeHasNoDebugOutput :: IORef Bool
+v_unsafeHasNoDebugOutput = global False
+
+{-# NOINLINE v_unsafeHasNoStateHack #-}
+v_unsafeHasNoStateHack :: IORef Bool
+v_unsafeHasNoStateHack = global False
+
 unsafeHasPprDebug :: Bool
-unsafeHasPprDebug = False
+unsafeHasPprDebug = unsafePerformIO $ readIORef v_unsafeHasPprDebug
+
+unsafeHasNoDebugOutput :: Bool
+unsafeHasNoDebugOutput = unsafePerformIO $ readIORef v_unsafeHasNoDebugOutput
+
+unsafeHasNoStateHack :: Bool
+unsafeHasNoStateHack = unsafePerformIO $ readIORef v_unsafeHasNoStateHack
+
+global :: a -> IORef a
+global a = unsafePerformIO (newIORef a)
