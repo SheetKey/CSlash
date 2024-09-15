aab1dae576a1c207c11637d59de6c80d7d98cce4
Will <wveatchw@gmail.com>
2024-09-15 16:44:06 -0400

    way helpers

--- src/CSlash/Platform/Ways.hs
+++ src/CSlash/Platform/Ways.hs
@@ -16,3 +16,26 @@ data Way
   deriving (Eq, Ord, Show)
 
 type Ways = Set Way
+
+addWay :: Way -> Ways -> Ways
+addWay = Set.insert
+
+waysTag :: Ways -> String
+waysTag = concat . intersperse "_" . map wayTag . Set.toAscList
+
+waysBuildTag :: Ways -> String
+waysBuildTag ws = waysTag (Set.filter (not . wayRTSOnly) ws)
+
+wayTag :: Way -> String
+wayTag (WayCustom xs) = xs
+wayTag WayThreaded = "thr"
+wayTag WayDebug = "debug"
+wayTag WayDyn = "dyn"
+wayTag WayProf = "p"
+
+wayRTSOnly :: Way -> Bool
+wayRTSOnly (WayCustom{}) = False
+wayRTSOnly WayDyn = False
+wayRTSOnly WayProf = False
+wayRTSOnly WayThreaded = True
+wayRTSOnly WayDebug = True
