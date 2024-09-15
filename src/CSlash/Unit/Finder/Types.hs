d9597557ff99ecf19f5eed7a5cf265eddfbf20aa
Will <wveatchw@gmail.com>
2024-09-15 16:45:12 -0400

    types

--- src/CSlash/Unit/Finder/Types.hs
+++ src/CSlash/Unit/Finder/Types.hs
@@ -23,3 +23,37 @@ data InstalledFindResult
   | InstalledNoPackage UnitId
   | InstalledNotFound [FilePath] (Maybe UnitId)
 
+data FindResult
+  = Found ModLocation Module
+  | NoPackage Unit
+  | FoundMultiple [(Module, ModuleOrigin)]
+  | NotFound
+    { fr_paths :: [FilePath]
+    , fr_pkg :: Maybe Unit
+    , fr_mods_hidden :: [Unit]
+    , fr_pkgs_hidden :: [Unit]
+    , fr_unusables :: [UnusableUnit]
+    , fr_suggestions :: [ModuleSuggestion]
+    }
+
+data FinderOpts = FinderOpts
+  { finder_importPaths :: [FilePath]
+  , finder_lookupHomeInterfaces :: Bool
+  , finder_bypassHiFileCheck :: Bool
+  , finder_ways :: Ways
+  , finder_enableSuggestions :: Bool
+  , finder_workingDirectory :: Maybe FilePath
+  , finder_thisPackageName :: Maybe FastString
+  , finder_hiddenModules :: Set.Set ModuleName
+  , finder_reexportedModules :: Set.Set ModuleName
+  , finder_hieDir :: Maybe FilePath
+  , finder_hieSuf :: String
+  , finder_hiDir :: Maybe FilePath
+  , finder_hiSuf :: String
+  , finder_dynHiSuf :: String
+  , finder_objectDir :: Maybe FilePath
+  , finder_objectSuf :: String
+  , finder_dynObjectSuf :: String
+  , finder_stubDir :: Maybe FilePath
+  }
+  deriving Show
