a90820faaca4446e9f704cf89ca910fe42647db8
Will <wveatchw@gmail.com>
2024-09-15 16:43:14 -0400

    cs_home_unit_maybe

--- src/CSlash/Driver/Env.hs
+++ src/CSlash/Driver/Env.hs
@@ -5,10 +5,10 @@ module CSlash.Driver.Env
   ) where
 
 import CSlash.Driver.DynFlags
--- import GHC.Driver.Errors ( printOrThrowDiagnostics )
+import CSlash.Driver.Errors ( printOrThrowDiagnostics )
 import CSlash.Driver.Errors.Types ( CsMessage )
--- import GHC.Driver.Config.Logger (initLogFlags)
--- import GHC.Driver.Config.Diagnostic (initDiagOpts, initPrintConfig)
+import CSlash.Driver.Config.Logger (initLogFlags)
+import CSlash.Driver.Config.Diagnostic (initDiagOpts, initPrintConfig)
 import CSlash.Driver.Env.Types ( Cs(..), CsEnv(..) )
 
 -- import GHC.Runtime.Context
@@ -19,15 +19,15 @@ import CSlash.Unit
 -- import GHC.Unit.Module.ModIface
 -- import GHC.Unit.Module.ModDetails
 -- import GHC.Unit.Home.ModInfo
--- import GHC.Unit.Env
--- import GHC.Unit.External
+import CSlash.Unit.Env
+import CSlash.Unit.External
 
 -- import GHC.Core         ( CoreRule )
 -- import GHC.Core.FamInstEnv
 -- import GHC.Core.InstEnv
 
 -- import GHC.Types.Annotations ( Annotation, AnnEnv, mkAnnEnv, plusAnnEnv )
--- import GHC.Types.CompleteMatch
+import CSlash.Types.CompleteMatch
 import CSlash.Types.Error ( emptyMessages, Messages )
 import CSlash.Types.Name
 import CSlash.Types.Name.Env
@@ -47,3 +47,6 @@ import CSlash.Utils.Logger
 import Data.IORef
 import qualified Data.Set as Set
 import CSlash.Unit.Module.Graph
+
+cs_home_unit_maybe :: CsEnv -> Maybe HomeUnit
+cs_home_unit_maybe = ue_homeUnit . cs_unit_env
