664470cfc95ce841639f52200e5f340b1ce83a05
Will <wveatchw@gmail.com>
2024-10-03 17:04:53 -0400

    outputable uniqmap instance

--- src/CSlash/Types/Unique/Map.hs
+++ src/CSlash/Types/Unique/Map.hs
@@ -1,3 +1,13 @@
 module CSlash.Types.Unique.Map ( module X ) where
 
 import GHC.Types.Unique.Map as X
+
+import CSlash.Types.Unique.FM (nonDetEltsUFM)
+
+import CSlash.Utils.Outputable
+
+instance (Outputable k, Outputable a) => Outputable (UniqMap k a) where
+  ppr (UniqMap m) =
+    brackets $ fsep $ punctuate comma $
+    [ ppr k <+> text "->" <+> ppr v
+    | (k, v) <- nonDetEltsUFM m ]
