a402ad745522c5a3a7f7f0e50fc535deb5d80d37
Will <wveatchw@gmail.com>
2024-10-06 13:26:18 -0400

    content

--- src/CSlash/Iface/Syntax.hs
+++ src/CSlash/Iface/Syntax.hs
@@ -5,14 +5,16 @@ module CSlash.Iface.Syntax
   , module CSlash.Iface.Syntax
   ) where
 
+import Prelude hiding ((<>))
+
 import CSlash.Data.FastString
 -- import CSlash.Builtin.Names ( unrestrictedFunTyConKey, liftedTypeKindTyConKey,
 --                               constraintKindTyConKey )
 import CSlash.Types.Unique ( hasKey )
 import CSlash.Iface.Type
--- import GHC.Iface.Recomp.Binary
--- import CSlash.Core( IsOrphan, isOrphan, UnfoldingCache(..) )
--- import GHC.Types.Demand
+import CSlash.Iface.Recomp.Binary
+import CSlash.Core( {-IsOrphan, isOrphan,-} UnfoldingCache(..) )
+import CSlash.Types.Demand
 -- import GHC.Types.Cpr
 -- import GHC.Core.Class
 -- import GHC.Types.FieldLabel
@@ -56,31 +58,128 @@ import Control.DeepSeq
 *                                                                      *
 ********************************************************************* -}
 
-type IfaceToppBndr = Name
+type IfaceTopBndr = Name
 
 data IfaceDecl
-  -- = IfaceId
-  --   { ifName :: IfaceTopBndr
-  --   , ifType :: IfaceType
-  --   , ifIdDetails :: IfaceIdDetails
-  --   , ifIdInfo :: IfaceIdInfo
-  --   }
+  = IfaceId
+    { ifName :: IfaceTopBndr
+    , ifType :: IfaceType
+    , ifIdDetails :: IfaceIdDetails
+    , ifIdInfo :: IfaceIdInfo
+    }
+  | IfaceData
+    { ifName :: IfaceTopBndr
+    , ifBinders :: [IfaceTyConBinder]
+    , ifResKind :: IfaceKind
+    , ifCons :: IfaceConDecls
+    }
+  | IfaceSynonym
+    { ifName :: IfaceTopBndr
+    , ifBinders :: [IfaceTyConBinder]
+    , ifResKind :: IfaceKind
+    , ifSynRhs :: IfaceType
+    }
+
+data IfaceConDecls
+  = IfAbstractTyCon
+  | IfDataTyCon [IfaceConDecl]
+
+data IfaceConDecl
+  = IfCon
+    { ifConName :: IfaceTopBndr
+    , ifConInfix :: Bool
+    , ifConTvBinders :: [IfaceBndr]
+    , ifConArgTys :: [IfaceType]
+    }
 
 data IfaceAnnotation
 
 data IfaceCompleteMatch
 
+instance Outputable IfaceCompleteMatch
+
 type IfaceIdInfo = [IfaceInfoItem]
 
 data IfaceInfoItem
+  = CsArity Arity
+  | CsDmdSig DmdSig
+  | CsInline InlinePragma
+  | CsUnfold Bool IfaceUnfolding
+  | CsNoCafRefs
+  | CsLFInfo IfaceLFInfo
+
+data IfaceUnfolding
+  = IfCoreUnfold UnfoldingSource
+                 IfUnfoldingCache
+                 IfGuidance
+                 IfaceExpr
+
+type IfUnfoldingCache = UnfoldingCache
+
+data IfGuidance
+  = IfNoGuidance
+  | IfWhen Arity Bool Bool
 
 data IfaceIdDetails
+  = IfVanillaId
+
+data IfaceLFInfo
+  = IfLFReEntrant !Arity
+  | IfLFCon !Name
+  | IfLFUnknown !Bool
+
+instance Outputable IfaceLFInfo where
+  ppr (IfLFReEntrant arity) = text "LFReEntrant" <+> ppr arity
+  ppr (IfLFCon con) = text "LFCon" <> brackets (ppr con)
+  ppr (IfLFUnknown fun_flag) = text "LFUnknown" <+> ppr fun_flag
+
+instance Binary IfaceLFInfo where
+    put_ bh (IfLFReEntrant arity) = do
+        putByte bh 0
+        put_ bh arity
+    put_ bh (IfLFCon con_name) = do
+        putByte bh 2
+        put_ bh con_name
+    put_ bh (IfLFUnknown fun_flag) = do
+        putByte bh 3
+        put_ bh fun_flag
+    get bh = do
+        tag <- getByte bh
+        case tag of
+            0 -> IfLFReEntrant <$> get bh
+            2 -> IfLFCon <$> get bh
+            3 -> IfLFUnknown <$> get bh
+            _ -> panic "Invalid byte"
+
 
 {- *********************************************************************
 *                                                                      *
                 Functions over declarations
 *                                                                      *
 ********************************************************************* -}
+
+ifaceDeclImplicitBndrs :: IfaceDecl -> [OccName]
+ifaceDeclImplicitBndrs (IfaceData { ifName = tc_name, ifCons = cons })
+  = case cons of
+      IfAbstractTyCon -> []
+      IfDataTyCon cds -> concatMap ifaceConDeclImplicitBndrs cds
+ifaceDeclImplicitBndrs _ = []
+
+ifaceConDeclImplicitBndrs :: IfaceConDecl -> [OccName]
+ifaceConDeclImplicitBndrs (IfCon { ifConName = con_name })
+  = [occName con_name, mkDataConWorkerOcc (occName con_name)]
+
+-- -----------------------------------------------------------------------------
+-- The fingerprints of an IfaceDecl
+
+ifaceDeclFingerprints :: Fingerprint -> IfaceDecl -> [(OccName, Fingerprint)]
+ifaceDeclFingerprints hash decl
+  = (getOccName decl, hash) :
+  [ (occ, computeFingerprint' (hash, occ))
+  | occ <- ifaceDeclImplicitBndrs decl ]
+  where
+    computeFingerprint' =
+      unsafeDupablePerformIO . computeFingerprint (panic "ifaceDeclFingerprints")
 
 {- *********************************************************************
 *                                                                      *
@@ -99,6 +198,57 @@ data IfaceBindingX r b
 
 data IfaceTopBndrInfo
   = IfLclTopBndr IfLclName IfaceType IfaceIdInfo IfaceIdDetails
-  | IfGblTopBndr IfaceToppBndr
+  | IfGblTopBndr IfaceTopBndr
 
 data IfaceMaybeRhs = IfUseUnfoldingRhs | IfRhs IfaceExpr
+
+{- *********************************************************************
+*                                                                      *
+              Printing IfaceDecl
+*                                                                      *
+********************************************************************* -}
+
+instance NamedThing IfaceConDecl where
+  getName = ifConName
+
+instance HasOccName IfaceConDecl where
+  occName = getOccName
+
+instance NamedThing IfaceDecl where
+  getName = ifName
+
+instance HasOccName IfaceDecl where
+  occName = getOccName
+
+instance Outputable IfaceDecl where
+  ppr = pprIfaceDecl showToIface
+
+instance (Outputable r, Outputable b) => Outputable (IfaceBindingX r b)
+
+instance Outputable IfaceTopBndrInfo
+
+instance Outputable IfaceMaybeRhs
+
+showToIface :: ShowSub
+showToIface = ShowSub ShowIface
+
+pprIfaceDecl :: ShowSub -> IfaceDecl -> SDoc
+pprIfaceDecl = undefined  
+
+{- *********************************************************************
+*                                                                      *
+                Binary instances
+*                                                                      *
+********************************************************************* -}
+
+instance Binary IfaceDecl
+
+instance Binary IfaceAnnotation
+
+instance (Binary r, Binary b) => Binary (IfaceBindingX b r)
+
+instance Binary IfaceTopBndrInfo
+
+instance Binary IfaceMaybeRhs
+
+instance Binary IfaceCompleteMatch
