8028146fe4cb7b76ba3d0e1c8b004c65573c8d8c
Will <wveatchw@gmail.com>
2024-10-06 13:28:03 -0400

    content

--- src/CSlash/Unit/Module/ModIface.hs
+++ src/CSlash/Unit/Module/ModIface.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 
@@ -13,7 +16,7 @@ import CSlash.Unit.Module.Warnings
 import CSlash.Types.Avail
 import CSlash.Types.Fixity
 import CSlash.Types.Fixity.Env
--- import GHC.Types.HpcInfo
+import CSlash.Types.PcInfo
 import CSlash.Types.Name
 import CSlash.Types.Name.Reader
 -- import GHC.Types.SafeHaskell
@@ -38,7 +41,7 @@ data ModIfaceBackend = ModIfaceBackend
   , mi_mod_hash :: !Fingerprint
   , mi_flag_hash :: !Fingerprint
   , mi_opt_hash :: !Fingerprint
-  , mi_hpc_hash :: !Fingerprint
+  , mi_pc_hash :: !Fingerprint
   , mi_exp_hash :: !Fingerprint
   , mi_fix_fn :: !(OccName -> Maybe Fixity)
   , mi_hash_fn :: !(OccName -> Maybe (OccName, Fingerprint))
@@ -67,11 +70,68 @@ data ModIface_ (phase :: ModIfacePhase) = ModIface
   , mi_decls :: [IfaceDeclExts phase]
   , mi_extra_decls :: Maybe [IfaceBindingX IfaceMaybeRhs IfaceTopBndrInfo]
   , mi_globals :: !(Maybe IfGlobalRdrEnv)
-  -- , mi_hpc :: !AnyHpcUsage
+  , mi_pc :: !AnyPcUsage
   , mi_complete_matches :: ![IfaceCompleteMatch]
   , mi_final_exts :: !(IfaceBackendExts phase)
-  -- , mi_ext_fields :: !ExtensibleFields
   , mi_src_hash :: !Fingerprint
   }
 
+instance Binary ModIface where
+  put_ bh (ModIface { mi_final_exts = ModIfaceBackend {..}, .. }) = do
+    put_ bh mi_module
+    put_ bh mi_cs_src
+    put_ bh mi_iface_hash
+    put_ bh mi_mod_hash
+    put_ bh mi_flag_hash
+    put_ bh mi_opt_hash
+    put_ bh mi_pc_hash
+    lazyPut bh mi_deps
+    lazyPut bh mi_usages
+    put_ bh mi_exports
+    put_ bh mi_exp_hash
+    put_ bh mi_fixities
+    lazyPut bh mi_anns
+    put_ bh mi_decls
+    put_ bh mi_extra_decls
+    put_ bh mi_pc
+    put_ bh mi_complete_matches
+
+  get bh = do
+    mi_module <- get bh
+    mi_cs_src <- get bh
+    mi_iface_hash <- get bh
+    mi_mod_hash <- get bh
+    mi_flag_hash <- get bh
+    mi_opt_hash <- get bh
+    mi_pc_hash <- get bh
+    mi_deps <- lazyGet bh
+    mi_usages <- {-# SCC "bin_usages" #-} lazyGet bh
+    mi_exports <- {-# SCC "bin_exports" #-} get bh
+    mi_exp_hash <- get bh
+    mi_fixities <- {-# SCC "bin_fixities" #-} get bh
+    mi_anns <- {-# SCC "bin_anns" #-} lazyGet bh
+    mi_decls <- {-# SCC "bin_tycldecls" #-} get bh
+    mi_extra_decls <- get bh
+    mi_pc <- get bh
+    mi_complete_matches <- get bh
+    return $ ModIface
+      { mi_src_hash = fingerprint0
+      , mi_globals = Nothing
+      , mi_final_exts = ModIfaceBackend
+                        { mi_fix_fn = mkIfaceFixCache mi_fixities
+                        , mi_hash_fn = mkIfaceHashCache mi_decls
+                        , ..
+                        }
+      , ..
+      }
+
 type IfaceExport = AvailInfo
+
+mkIfaceHashCache :: [(Fingerprint, IfaceDecl)] -> OccName -> Maybe (OccName, Fingerprint)
+mkIfaceHashCache pairs
+  = \occ -> lookupOccEnv env occ
+  where
+    env = foldl' add_decl emptyOccEnv pairs
+    add_decl env0 (v, d) = foldl' add env0 (ifaceDeclFingerprints v d)
+      where
+        add env0 (occ, hash) = extendOccEnv env0 occ (occ, hash)
