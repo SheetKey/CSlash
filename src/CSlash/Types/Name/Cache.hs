ccdf7dfd7c106d3c7052d526373c6fe2e19af1fc
Will <wveatchw@gmail.com>
2024-10-06 13:27:32 -0400

    cache

--- src/CSlash/Types/Name/Cache.hs
+++ src/CSlash/Types/Name/Cache.hs
@@ -20,6 +20,19 @@ data NameCache = NameCache
 
 type OrigNameCache = ModuleEnv (OccEnv Name)
 
+takeUniqFromNameCache :: NameCache -> IO Unique
+takeUniqFromNameCache (NameCache c _) = uniqFromTag c
+
+lookupOrigNameCache :: OrigNameCache -> Module -> OccName -> Maybe Name
+lookupOrigNameCache nc mod occ
+  | mod == cSLASH_TYPES || mod == cSLASH_PRIM 
+  , Just name <- isBuiltInOcc_maybe occ <|> isPunOcc_maybe mod occ
+  = Just name
+  | otherwise
+  = case lookupModuleEnv nc mod of
+      Nothing -> Nothing
+      Just occ_env -> lookupOccEnv occ_env occ
+
 extendOrigNameCache' :: OrigNameCache -> Name -> OrigNameCache
 extendOrigNameCache' nc name
   = assertPpr (isExternalName name) (ppr name) $
@@ -36,3 +49,12 @@ initNameCache c names = NameCache c <$> newMVar (initOrigNames names)
 
 initOrigNames :: [Name] -> OrigNameCache
 initOrigNames names = foldl' extendOrigNameCache' emptyModuleEnv names
+
+updateNameCache'
+  :: NameCache
+  -> (OrigNameCache -> IO (OrigNameCache, c))
+  -> IO c
+updateNameCache' (NameCache _ nc) upd_fn = modifyMVar' nc upd_fn
+
+modifyMVar' :: MVar a -> (a -> IO (a, b)) -> IO b
+modifyMVar' m f = modifyMVar m $ f >=> \c -> fst c `seq` pure c
