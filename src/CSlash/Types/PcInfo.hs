ba386e51526937c06e5327185e38e403b03395d1
Will <wveatchw@gmail.com>
2024-10-07 16:59:27 -0400

    pcinfo

--- src/CSlash/Types/PcInfo.hs
+++ src/CSlash/Types/PcInfo.hs
@@ -1,3 +1,12 @@
 module CSlash.Types.PcInfo where
 
+data PcInfo
+  = PcInfo
+    { pcInfoTickCount :: Int
+    , pcInfoHash :: Int
+    }
+  | NoPcInfo
+    { pcUsed :: AnyPcUsage
+    }
+
 type AnyPcUsage = Bool
