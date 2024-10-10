ef1a08a55e845f9d9cd6a314e0210a8e19883112
Will <wveatchw@gmail.com>
2024-10-10 17:29:08 -0400

    init iface stuff

--- src/CSlash/Tc/Utils/Monad.hs
+++ src/CSlash/Tc/Utils/Monad.hs
@@ -1,6 +1,109 @@
 module CSlash.Tc.Utils.Monad
-  (
-    module CSlash.Tc.Types
+  ( module CSlash.Tc.Utils.Monad
+  , module CSlash.Tc.Types
+  , module CSlash.Data.IOEnv
   ) where
 
-import CSlash.Tc.Types
+import CSlash.Builtin.Names
+
+-- import GHC.Tc.Errors.Types
+import CSlash.Tc.Types     -- Re-export all
+-- import GHC.Tc.Types.Constraint
+-- import GHC.Tc.Types.Evidence
+import GHC.Tc.Types.Origin
+import CSlash.Tc.Types.TcRef
+import GHC.Tc.Utils.TcType
+-- import GHC.Tc.Zonk.TcType
+
+import CSlash.Cs hiding (LIE)
+
+import CSlash.Unit
+import CSlash.Unit.Env
+import CSlash.Unit.External
+import CSlash.Unit.Module.Warnings
+import CSlash.Unit.Home.ModInfo
+
+-- import GHC.Core.UsageEnv
+-- import GHC.Core.Multiplicity
+-- import GHC.Core.InstEnv
+-- import GHC.Core.FamInstEnv
+
+import CSlash.Driver.Env
+import CSlash.Driver.Session
+import CSlash.Driver.Config.Diagnostic
+
+-- import GHC.Runtime.Context
+
+import CSlash.Data.IOEnv -- Re-export all
+import CSlash.Data.Bag
+import CSlash.Data.FastString
+import CSlash.Data.Maybe
+
+import CSlash.Utils.Outputable as Outputable
+import CSlash.Utils.Error
+import CSlash.Utils.Panic
+import CSlash.Utils.Constants (debugIsOn)
+import CSlash.Utils.Logger
+import qualified CSlash.Data.Strict as Strict
+
+import CSlash.Types.Error
+import CSlash.Types.Fixity.Env
+import CSlash.Types.Name.Reader
+import CSlash.Types.Name
+-- import GHC.Types.SafeHaskell
+import CSlash.Types.Id
+import CSlash.Types.TypeEnv
+import CSlash.Types.Var.Set
+import CSlash.Types.Var.Env
+import CSlash.Types.SrcLoc
+import CSlash.Types.Name.Env
+import CSlash.Types.Name.Set
+-- import CSlash.Types.Name.Ppr
+import CSlash.Types.Unique.FM ( emptyUFM )
+import CSlash.Types.Unique.Supply
+-- import GHC.Types.Annotations
+import CSlash.Types.Basic( TopLevelFlag{-, TypeOrKind(..)-} )
+-- import GHC.Types.CostCentre.State
+import CSlash.Types.SourceFile
+
+-- import qualified GHC.LanguageExtensions as LangExt
+
+import Data.IORef
+import Control.Monad
+
+import qualified Data.Map as Map
+import CSlash.Driver.Env.KnotVars
+import CSlash.Linker.Types
+import CSlash.Types.Unique.DFM
+-- import CSlash.Iface.Errors.Types
+-- import GHC.Iface.Errors.Ppr
+import CSlash.Tc.Types.LclEnv
+
+{- *********************************************************************
+*                                                                      *
+                Initialisation
+*                                                                      *
+********************************************************************* -}
+
+initTcRnIf :: Char -> CsEnv -> gbl -> lcl -> TcRnIf gbl lcl a -> IO a
+initTcRnIf uniq_tag cs_env gbl_env lcl_env thing_inside =
+  let env = Env { env_top = cs_env
+                , env_ut = uniq_tag
+                , env_gbl = gbl_env
+                , env_lcl = lcl_env
+                } 
+  in runIOEnv env thing_inside
+
+{- *********************************************************************
+*                                                                      *
+             Stuff for interface decls
+*                                                                      *
+********************************************************************* -}
+
+initIfaceCheck :: SDoc -> CsEnv -> IfG a -> IO a
+initIfaceCheck doc cs_env do_this =
+  let gbl_env = IfGblEnv
+                { if_doc = text "initIfaceCheck" <+> doc
+                , if_rec_types = readTcRef <$> cs_type_env_vars cs_env
+                }
+  in initTcRnIf 'i' cs_env gbl_env () do_this
