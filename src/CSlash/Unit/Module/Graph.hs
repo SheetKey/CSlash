50e7d698a231689886161533c07cd19c515ad080
Will <wveatchw@gmail.com>
2024-10-13 17:43:33 -0400

    graph stuff

--- src/CSlash/Unit/Module/Graph.hs
+++ src/CSlash/Unit/Module/Graph.hs
@@ -27,7 +27,7 @@ import CSlash.Types.Unique.DSet
 import qualified Data.Set as Set
 import Data.Set (Set)
 import CSlash.Unit.Module
--- import GHC.Linker.Static.Utils
+import CSlash.Linker.Static.Utils
 
 import Data.Bifunctor
 import Data.Function
@@ -47,6 +47,12 @@ moduleGraphNodeModSum (InstantiationNode {}) = Nothing
 moduleGraphNodeModSum (LinkNode {}) = Nothing
 moduleGraphNodeModSum (ModuleNode _ ms) = Just ms
 
+moduleGraphNodeUnitId :: ModuleGraphNode -> UnitId
+moduleGraphNodeUnitId mgn =
+  case mgn of
+    InstantiationNode uid _iud -> uid
+    ModuleNode _ ms -> toUnitId (moduleUnit (ms_mod ms))
+    LinkNode _ uid -> uid
 
 instance Outputable ModuleGraphNode where
   ppr = \case
@@ -73,6 +79,10 @@ pprNodeKey :: NodeKey -> SDoc
 pprNodeKey (NodeKey_Unit iu) = ppr iu
 pprNodeKey (NodeKey_Module mk) = ppr mk
 pprNodeKey (NodeKey_Link uid) = ppr uid
+
+nodeKeyModName :: NodeKey -> Maybe ModuleName
+nodeKeyModName (NodeKey_Module mk) = Just (mnkModuleName mk)
+nodeKeyModName _ = Nothing
 
 data ModNodeKeyWithUid = ModNodeKeyWithUid
   { mnkModuleName :: !ModuleName
@@ -104,13 +114,92 @@ unionMG a b =
      , mg_trans_deps = mkTransDeps new_mss
      }
 
+mgModSummaries :: ModuleGraph -> [ModSummary]
+mgModSummaries mg = [ m | ModuleNode _ m <- mgModSummaries' mg ]
+
+mgModSummaries' :: ModuleGraph -> [ModuleGraphNode]
+mgModSummaries' = mg_mss
+
+mgLookupModule :: ModuleGraph -> Module -> Maybe ModSummary
+mgLookupModule ModuleGraph{..} m = listToMaybe $ mapMaybe go mg_mss
+  where
+    go (ModuleNode _ ms)
+      | ms_mod ms == m
+      = Just ms
+    go _ = Nothing
+
 emptyMG :: ModuleGraph
 emptyMG = ModuleGraph [] Map.empty
+
+extendMG :: ModuleGraph -> [NodeKey] -> ModSummary -> ModuleGraph
+extendMG ModuleGraph{..} deps ms = ModuleGraph
+  { mg_mss = ModuleNode deps ms : mg_mss
+  , mg_trans_deps = mkTransDeps (ModuleNode deps ms : mg_mss)
+  }
 
 mkTransDeps :: [ModuleGraphNode] -> Map.Map NodeKey (Set.Set NodeKey)
 mkTransDeps mss =
   let (gg, _) = moduleGraphNodes mss
   in allReachable gg (mkNodeKey . node_payload)
+
+extendMGInst :: ModuleGraph -> UnitId -> InstantiatedUnit -> ModuleGraph
+extendMGInst mg uid depUnitId = mg { mg_mss = InstantiationNode uid depUnitId : mg_mss mg }
+
+extendMGLink :: ModuleGraph -> UnitId -> [NodeKey] -> ModuleGraph
+extendMGLink mg uid nks = mg { mg_mss = LinkNode nks uid : mg_mss mg }
+
+extendMG' :: ModuleGraph -> ModuleGraphNode -> ModuleGraph
+extendMG' mg (InstantiationNode uid depUnitId) = extendMGInst mg uid depUnitId
+extendMG' mg (ModuleNode deps ms) = extendMG mg deps ms
+extendMG' mg (LinkNode deps uid) = extendMGLink mg uid deps
+
+mkModuleGraph :: [ModuleGraphNode] -> ModuleGraph
+mkModuleGraph = foldr (flip extendMG') emptyMG
+
+filterToposortToModules :: [SCC ModuleGraphNode] -> [SCC ModSummary]
+filterToposortToModules = mapMaybe $ mapMaybeSCC $ \case
+  InstantiationNode _ _ -> Nothing
+  LinkNode {} -> Nothing
+  ModuleNode _ node -> Just node
+  where
+    mapMaybeSCC :: (a -> Maybe b) -> SCC a -> Maybe (SCC b)
+    mapMaybeSCC f = \case
+      AcyclicSCC a -> AcyclicSCC <$> f a
+      CyclicSCC as -> case mapMaybe f as of
+        [] -> Nothing
+        [a] -> Just $ AcyclicSCC a
+        as -> Just $ CyclicSCC as
+
+showModMsg :: DynFlags -> Bool -> ModuleGraphNode -> SDoc
+showModMsg dflags _ (LinkNode {}) =
+  let staticLink = case csLink dflags of
+                     LinkStaticLib -> True
+                     _ -> False
+      platform = targetPlatform dflags
+      arch_os = platformArchOS platform
+      prog_file = progFileName arch_os staticLink (outputFile_ dflags)
+  in text prog_file
+showModMsg _ _ (InstantiationNode _ indef_unit) =
+  ppr $ instUnitInstanceOf indef_unit
+showModMsg dflags recomp (ModuleNode _ mod_summary) =
+  if gopt Opt_HideSourcePaths dflags
+  then text mod_str
+  else hsep $ [ text (mod_str ++ replicate (max 0 (16 - length mod_str)) ' ')
+              , char '('
+              , text (op $ msCsFilePath mod_summary) <> char ','
+              , message
+              , char ')' ]
+  where
+    op = normalise
+    mod_str = moduleNameString (moduleName (ms_mod mod_summary))
+              ++ csSourceString (ms_cs_src mod_summary)
+    dyn_file = op $ msDynObjFilePath mod_summary
+    obj_file = op $ msObjFilePath mod_summary
+    files = [ obj_file ]
+            ++ [ dyn_file | gopt Opt_BuildDynamicToo dflags ]
+    message = case backendSpecialModuleSource (backend dflags) of
+                Just special -> text special
+                Nothing -> foldr1 (\ofile rest -> ofile <> comma <+> rest) (map text files)
 
 type SummaryNode = Node Int ModuleGraphNode
 
