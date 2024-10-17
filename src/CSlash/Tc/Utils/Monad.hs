088949e1efd3cb2163056088a4b770a498ce9b76
Will <wveatchw@gmail.com>
2024-10-17 12:38:25 -0400

    tcrn monad

--- src/CSlash/Tc/Utils/Monad.hs
+++ src/CSlash/Tc/Utils/Monad.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module CSlash.Tc.Utils.Monad
   ( module CSlash.Tc.Utils.Monad
   , module CSlash.Tc.Types
@@ -96,9 +98,90 @@ initTcRnIf uniq_tag cs_env gbl_env lcl_env thing_inside =
 
 {- *********************************************************************
 *                                                                      *
+                Simple accessors
+*                                                                      *
+********************************************************************* -}
+
+getTopEnv :: TcRnIf gbl lcl CsEnv
+getTopEnv = do { env <- getEnv; return (env_top env) }
+
+updTopEnv :: (CsEnv -> CsEnv) -> TcRnIf gbl lcl a -> TcRnIf gbl lcl a
+updTopEnv upd = updEnv (\env@(Env { env_top = top }) -> env { env_top = upd top })
+
+getGblEnv :: TcRnIf gbl lcl gbl
+getGblEnv = do
+  Env{..} <- getEnv
+  return env_gbl
+
+updGblEnv :: (gbl -> gbl) -> TcRnIf gbl lcl a -> TcRnIf gbl lcl a
+updGblEnv upd = updEnv (\env@(Env { env_gbl = gbl }) -> env { env_gbl = upd gbl })
+
+setLclEnv :: lcl' -> TcRnIf gbl lcl' a -> TcRnIf gbl lcl a
+setLclEnv lcl_env = updEnv (\env -> env { env_lcl = lcl_env })
+
+doptM :: DumpFlag -> TcRnIf gbl lcl Bool
+doptM flag = do
+  logger <- getLogger
+  return (logHasDumpFlag logger flag)
+
+goptM :: GeneralFlag -> TcRnIf gbl lcl Bool
+goptM flag = gopt flag <$> getDynFlags
+
+whenDOptM :: DumpFlag -> TcRnIf gbl lcl () -> TcRnIf gbl lcl ()
+whenDOptM flag thing_inside = do
+  b <- doptM flag
+  when b thing_inside
+
+getEpsVar :: TcRnIf gbl lcl (TcRef ExternalPackageState)
+getEpsVar = do
+  env <- getTopEnv
+  return (euc_eps (ue_eps (cs_unit_env env)))
+
+updateEps :: (ExternalPackageState -> (ExternalPackageState, a)) -> TcRnIf gbl lcl a
+updateEps upd = do
+  traceIf (text "updating EPS")
+  eps_var <- getEpsVar
+  atomicUpdMutVar' eps_var upd
+
+updateEps_ :: (ExternalPackageState -> ExternalPackageState) -> TcRnIf gbl lcl ()
+updateEps_ upd = updateEps (\eps -> (upd eps, ()))
+
+getEpsAndHug :: TcRnIf gbl lcl (ExternalPackageState, HomeUnitGraph)
+getEpsAndHug = do
+  env <- getTopEnv
+  eps <- liftIO $ csEPS env
+  return (eps, cs_HUG env)
+
+{- *********************************************************************
+*                                                                      *
+                Debugging
+*                                                                      *
+********************************************************************* -}
+
+traceIf :: SDoc -> TcRnIf m n ()
+traceIf = traceOptIf Opt_D_dump_if_trace
+{-# INLINE traceIf #-}
+
+traceOptIf :: DumpFlag -> SDoc -> TcRnIf m n ()
+traceOptIf flag doc = whenDOptM flag $ do
+  logger <- getLogger
+  liftIO (putMsg logger doc)
+
+{- *********************************************************************
+*                                                                      *
              Stuff for interface decls
 *                                                                      *
 ********************************************************************* -}
+
+mkIfLclEnv :: Module -> SDoc -> IfLclEnv
+mkIfLclEnv mod loc = IfLclEnv
+  { if_mod = mod
+  , if_loc = loc
+  , if_nsubst = Nothing
+  , if_implicits_env = Nothing
+  , if_tv_env = emptyFsEnv
+  , if_id_env = emptyFsEnv
+  }
 
 initIfaceCheck :: SDoc -> CsEnv -> IfG a -> IO a
 initIfaceCheck doc cs_env do_this =
@@ -107,3 +190,7 @@ initIfaceCheck doc cs_env do_this =
                 , if_rec_types = readTcRef <$> cs_type_env_vars cs_env
                 }
   in initTcRnIf 'i' cs_env gbl_env () do_this
+
+initIfaceLcl :: Module -> SDoc -> IfL a -> IfM lcl a
+initIfaceLcl mod loc_doc thing_inside =
+  setLclEnv (mkIfLclEnv mod loc_doc) thing_inside
