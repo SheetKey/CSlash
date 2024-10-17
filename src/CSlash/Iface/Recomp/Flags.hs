d5706eae2bc4cbde2dbc898bd19b0bf9e0af2ddf
Will <wveatchw@gmail.com>
2024-10-17 12:32:37 -0400

    iface.recomp.flags

---
+++ src/CSlash/Iface/Recomp/Flags.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module CSlash.Iface.Recomp.Flags where
+
+import CSlash.Driver.Session
+import CSlash.Driver.Env
+
+import CSlash.Utils.Binary
+import CSlash.Unit.Module
+import CSlash.Types.Name
+-- import GHC.Types.SafeHaskell
+import CSlash.Utils.Fingerprint
+import CSlash.Iface.Recomp.Binary
+import CSlash.Core.Opt.CallerCC ()
+
+import CSlash.Data.EnumSet as EnumSet
+import System.FilePath (normalise)
+
+import Debug.Trace (trace)
+
+fingerprintDynFlags :: CsEnv -> Module -> (BinHandle -> Name -> IO ()) -> IO Fingerprint
+fingerprintDynFlags cs_env this_mod nameio =
+  let dflags@DynFlags{..} = cs_dflags cs_env
+      mainis = if mainModIs (cs_HUE cs_env) == this_mod then Just mainFunIs else Nothing
+
+      includePathsMinusImplicit = includePaths { includePathsQuoteImplicit = [] }
+
+      flags = (mainis, (debugLevel, callerCcFilters))
+  in computeFingerprint nameio flags
+
+fingerprintOptFlags :: DynFlags -> (BinHandle -> Name -> IO ()) -> IO Fingerprint
+fingerprintOptFlags DynFlags{..} nameio =
+  let opt_flags = map fromEnum $ filter (`EnumSet.member` optimisationFlags)
+                                        (EnumSet.toList generalFlags)
+  in computeFingerprint nameio opt_flags
+
+fingerprintPcFlags :: DynFlags -> (BinHandle -> Name -> IO ()) -> IO Fingerprint
+fingerprintPcFlags dflags@DynFlags{..} nameio =
+  let pc = if gopt Opt_Pc dflags then Just pcDir else Nothing
+  in computeFingerprint nameio pc
