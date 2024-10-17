d5b980fe3e471b96dd1894ce0343364670c06d57
Will <wveatchw@gmail.com>
2024-10-17 12:33:25 -0400

    instances

--- src/CSlash/Data/IOEnv.hs
+++ src/CSlash/Data/IOEnv.hs
@@ -1,3 +1,19 @@
 module CSlash.Data.IOEnv ( module X ) where
 
 import GHC.Data.IOEnv as X
+
+import CSlash.Driver.DynFlags
+import CSlash.Utils.Logger
+import CSlash.Unit.Module
+
+instance ContainsDynFlags env => HasDynFlags (IOEnv env) where
+    getDynFlags = do env <- getEnv
+                     return $! extractDynFlags env
+
+instance ContainsLogger env => HasLogger (IOEnv env) where
+    getLogger = do env <- getEnv
+                   return $! extractLogger env
+
+-- instance ContainsModule env => HasModule (IOEnv env) where
+--     getModule = do env <- getEnv
+--                    return $ extractModule env
