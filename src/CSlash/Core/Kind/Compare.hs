41e2651eabde101b14deee5991eb5b758590d510
Will <wveatchw@gmail.com>
2025-01-10 14:10:04 -0500

    kind comparison

--- src/CSlash/Core/Kind/Compare.hs
+++ src/CSlash/Core/Kind/Compare.hs
@@ -3,6 +3,7 @@
 module CSlash.Core.Kind.Compare where
 
 import CSlash.Core.Kind
+import CSlash.Core.Kind.FVs
 
 import CSlash.Core.Type.Rep
 import CSlash.Core.Type.FVs
@@ -29,6 +30,22 @@ import qualified Data.Semigroup as S
 tcEqKind :: HasDebugCallStack => Kind -> Kind -> Bool
 tcEqKind orig_ki1 orig_ki2 = go orig_env orig_ki1 orig_ki2
   where
---    go :: RnEnv2 -> Kind -> Kind -> Bool
-    go = panic "tcEqKind go"
-    orig_env = panic "tcEqKind oe"
+    go :: RnEnv2 -> Kind -> Kind -> Bool
+    go _ UKd UKd = True
+    go _ AKd AKd = True
+    go _ LKd LKd = True
+    go env (KiVarKi kv1) (KiVarKi kv2)
+      = rnOccL env kv1 == rnOccR env kv2
+    go env (FunKd _ arg1 res1) (FunKd _ arg2 res2)
+      = go env arg1 arg2 && go env res1 res2
+    go env (KdContext rels1) (KdContext rels2)
+      = all2 (go_rel env) rels1 rels2
+    go _ _ _ = False
+
+    go_rel env (LTKd k1 k2) (LTKd k1' k2')
+      = go env k1 k1' && go env k2 k2'
+    go_rel env (LTEQKd k1 k2) (LTEQKd k1' k2')
+      = go env k1 k1' && go env k2 k2'
+    go_rel _ _ _ = False
+
+    orig_env = mkRnEnv2 $ mkInScopeSet $ kiVarsOfKinds [orig_ki1, orig_ki2]
