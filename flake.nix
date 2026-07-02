91694d4de0411fc6c2e5da279e02ce2c1d656c4b
Will <wveatchw@gmail.com>
2026-07-01 20:14:19 -0400

    install llvm files

--- flake.nix
+++ flake.nix
@@ -27,27 +27,39 @@
         jailbreakUnbreak = pkg:
           pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
 
+        my-pkg = haskellPackages.callCabal2nix packageName self { };
+        my-post-install-phase = ''
+          cp llvm-targets $out/lib
+          cp llvm-passes $out/lib
+        '';
+        my-pkg-post-install-files =
+          pkgs.haskell.lib.overrideCabal my-pkg
+            (old_pkg: { postInstall = (old_pkg.postInstall or "") + my-post-install-phase; });
+            
       in {
-        packages.${packageName} = # (ref:haskell-package-def)
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
-            # Ex with gi-gtk-declarative:
-            # If version is broken then:
-            # gi-gtk-declarative = jailbreakUnbreak haskeppPackages.gi-gtk-declarative;
-            # or if tests failing: 
-            # gi-gtk-declarative = pkgs.haskell.lib.dontCheck haskellPackages.gi-gtk-declarative;
+        # OLD
+        # packages.${packageName} = # (ref:haskell-package-def)
+        #   haskellPackages.callCabal2nix packageName self rec {
+        #     # Dependency overrides go here
+        #     # Ex with gi-gtk-declarative:
+        #     # If version is broken then:
+        #     # gi-gtk-declarative = jailbreakUnbreak haskeppPackages.gi-gtk-declarative;
+        #     # or if tests failing: 
+        #     # gi-gtk-declarative = pkgs.haskell.lib.dontCheck haskellPackages.gi-gtk-declarative;
 
-            #example = haskellPackages.callCabal2nix "example" example-input { };
+        #     #example = haskellPackages.callCabal2nix "example" example-input { };
 
-            # ghc = pkgs.haskellPackages.ghc_9_10_1.override {
-            #   ghc-boot = haskellPackages.ghc_9_10_1;
-            #   ghc-heap = haskellPackages.ghc-heap_9_10_1;
-            #   template-haskell = haskellPackages.template-haskell_2_22_0_0.override {
-            #     ghc-boot-th = haskellPackages.ghc-boot-th_9_10_1;
-            #   };
-            # };
-            #base = haskellPackages.base_4_20_0_0;
-          };
+        #     # ghc = pkgs.haskellPackages.ghc_9_10_1.override {
+        #     #   ghc-boot = haskellPackages.ghc_9_10_1;
+        #     #   ghc-heap = haskellPackages.ghc-heap_9_10_1;
+        #     #   template-haskell = haskellPackages.template-haskell_2_22_0_0.override {
+        #     #     ghc-boot-th = haskellPackages.ghc-boot-th_9_10_1;
+        #     #   };
+        #     # };
+        #     #base = haskellPackages.base_4_20_0_0;
+        #   };
+        # END OLD
+        packages.${packageName} = my-pkg-post-install-files;
 
         defaultPackage = self.packages.${system}.${packageName};
 
@@ -59,6 +71,8 @@
             # haskellPackages.implicit-hie
             haskellPackages.alex
             haskellPackages.happy
+
+            llvm_18
           ];
           inputsFrom = builtins.attrValues self.packages.${system};
         };
